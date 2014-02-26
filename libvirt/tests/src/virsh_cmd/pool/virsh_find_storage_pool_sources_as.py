f9214b97f0f0c31bb49f57db6666a147b1ba8f16
John Ferlan <jferlan@redhat.com>
2014-02-26 16:36:37 -0500

    Add try/except for login_iscsi and create_vg failure
    Add a try/except for login_iscsi or create_vg failure.  If the login_iscsi
    fails, then we leave with the iscsiadm still logged in if the creation
    of the vg fails.  If either fails, then use the exception to propagate
    the error.

--- libvirt/tests/src/virsh_cmd/pool/virsh_find_storage_pool_sources_as.py
+++ libvirt/tests/src/virsh_cmd/pool/virsh_find_storage_pool_sources_as.py
@@ -45,12 +45,17 @@ def run(test, params, env):
                 raise error.TestNAError("Command 'tgtadm' and/or 'iscsiadm' "
                                         "is missing. You must install it.")
             # Set up iscsi
-            iscsi_device = utils_test.libvirt.setup_or_cleanup_iscsi(True)
-            cleanup_iscsi = True
-            if source_type == "logical":
-                # Create VG by using iscsi device
-                lv_utils.vg_create(vg_name, iscsi_device)
-                cleanup_logical = True
+            try:
+                iscsi_device = utils_test.libvirt.setup_or_cleanup_iscsi(True)
+                cleanup_iscsi = True
+                if source_type == "logical":
+                    # Create VG by using iscsi device
+                    lv_utils.vg_create(vg_name, iscsi_device)
+                    cleanup_logical = True
+            except Exception, detail:
+                if cleanup_iscsi:
+                    utils_test.libvirt.setup_or_cleanup_iscsi(False)
+                raise error.TestFail("iscsi setup failed:\n%s" % detail)
 
     # Run virsh cmd
     options = "%s %s " % (source_host, source_port) + options
