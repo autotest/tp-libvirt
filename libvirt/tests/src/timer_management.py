fba5b905a1be4434fafab48dd76a0c1c1ade402c
Yu Mingfei <yumingfei@cn.fujitsu.com>
2014-03-26 17:16:45 +0900

    libvirt.tests: Add windows support for timer management.
    Signed-off-by: Yu Mingfei <yumingfei@cn.fujitsu.com>

--- libvirt/tests/src/timer_management.py
+++ libvirt/tests/src/timer_management.py
@@ -124,14 +124,18 @@ def recover_vm_xml(vms):
             logging.error(detail)
 
 
-def get_vm_time(vm, time_type=None):
+def get_vm_time(vm, time_type=None, windows=False):
     """
-    Return epoch time.
+    Return epoch time. Windows will return timezone only.
 
     :param time_type: UTC or timezone time
     """
     if time_type == "utc":
         cmd = "date -u +%s"
+    elif windows is True:
+        time_type == "tz"
+        cmd = (r"echo %date:~0,4%/%date:~5,2%/%date:~8,2%/"
+               "%time:~0,2%/%time:~3,2%/%time:~6,2%")
     else:
         cmd = "date +%Y/%m/%d/%H/%M/%S"
     session = vm.wait_for_login()
@@ -140,9 +144,15 @@ def get_vm_time(vm, time_type=None):
     if ts:
         logging.error("Get time in vm failed:%s", timestr)
         return -1
+    # To avoid some unexpected space, strip it manually
     if time_type == "utc":
         return int(timestr)
     else:
+        # Strip potential space in timestr(for windows)
+        elems = timestr.split('/')
+        timestr = "%s/%s/%s/%s/%s/%s" % (elems[0].strip(), elems[1].strip(),
+                                         elems[2].strip(), elems[3].strip(),
+                                         elems[4].strip(), elems[5].strip())
         return int(time.mktime(time.strptime(timestr.strip(),
                                              '%Y/%m/%d/%H/%M/%S')))
 
@@ -175,6 +185,22 @@ def set_vm_timezone(vm, timezone="America/New_York"):
             session.close()
             raise error.TestError("Set timezone failed:%s", to)
     session.close()
+
+
+def set_windows_timezone(vm, timezone="America/New_York"):
+    """Set windows vm timezone to what we want"""
+    timezone_codes = {"America/New_York": "Eastern Standard Time",
+                      "Europe/London": "UTC",
+                      "Asia/Shanghai": "China Standard Time",
+                      "Asia/Tokyo": "Tokyo Standard Time"}
+    if timezone not in timezone_codes.keys():
+        raise error.TestError("Not supported timezone, please add it.")
+    cmd = "tzutil /s \"%s\"" % timezone_codes[timezone]
+    session = vm.wait_for_login()
+    ts, to = session.cmd_status_output(cmd)
+    session.close()
+    if ts:
+        raise error.TestError("Set timezone failed:%s", to)
 
 
 def convert_tz_to_vector(tz_name="Europe/London"):
@@ -445,6 +471,89 @@ def test_banned_timer(vms, params):
             if get_current_clksrc(vm) == banned_timer:
                 fail_info.append("Set clocksource %s in vm successfully,"
                                  "but not expected." % banned_timer)
+
+    if len(fail_info):
+        raise error.TestFail(fail_info)
+
+
+def test_windows_timer(vms, params):
+    """
+    Test timers for windows.
+    """
+    host_tz = params.get("host_timezone", "Asia/Tokyo")
+    vm_tz = params.get("vm_timezone", "America/New_York")
+    clock_tz = params.get("clock_timezone", "Asia/Shanghai")
+    host_tz_vector = convert_tz_to_vector(host_tz)
+    vm_tz_vector = convert_tz_to_vector(vm_tz)
+    set_tz_vector = convert_tz_to_vector(clock_tz)
+    if ((host_tz_vector is None) or (vm_tz_vector is None)
+       or (set_tz_vector is None)):
+        raise error.TestError("Not supported timezone to convert.")
+    delta = int(params.get("allowd_delta", "300"))
+
+    # Confirm vm is down for editing
+    for vm in vms:
+        if vm.is_alive():
+            vm.destroy()
+
+    # Config clock in VMXML
+    set_xml_clock(vms, params)
+
+    # Logging vm to set time
+    for vm in vms:
+        vm.start()
+        vm.wait_for_login()
+        set_windows_timezone(vm, params.get("vm_timezone"))
+
+    # Set host timezone
+    set_host_timezone(params.get("host_timezone"))
+
+    # Get expected utc distance between host and vms
+    # with different offset(seconds)
+    offset = params.get("clock_offset", "utc")
+    # No matter what utc is, the timezone distance
+    vm_tz_span = vm_tz_vector * 3600
+    host_tz_span = host_tz_vector * 3600
+    if offset == "utc":
+        utc_span = 0
+    elif offset == "localtime":
+        utc_span = host_tz_vector * 3600
+    elif offset == "timezone":
+        utc_span = set_tz_vector * 3600
+    elif offset == "variable":
+        utc_span = int(params.get("clock_adjustment", 3600))
+
+    # TODO: It seems that actual timezone time in vm is only based on
+    # timezone on host. I need to confirm whether it is normal(or bug)
+    vm_tz_span = utc_span - host_tz_span
+
+    # To track failed information
+    fail_info = []
+
+    # Set vms' clocksource(different vm may have different sources)
+    # (kvm-clock tsc hpet acpi_pm...)
+    for vm in vms:
+        # Get windows vm's time(timezone)
+        vm_tz_tm = get_vm_time(vm, "tz", windows=True)
+        # Get host's utc time
+        host_utc_tm = int(time.time())
+
+        logging.debug("\nTimeZone time in vm:%s\n"
+                      "UTC time on host:%s\n",
+                      vm_tz_tm, host_utc_tm)
+
+        # Check got time
+        # Distance between utc time on host and timezone time in vm
+        vm_tz_distance = vm_tz_tm - host_utc_tm
+        logging.debug("\nVM timezone distance:%s\n"
+                      "Expected VM timezone distance:%s",
+                      vm_tz_distance, vm_tz_span)
+        # Check timezone time of vm
+        if abs(vm_tz_distance - vm_tz_span) > delta:
+            fail_info.append("Timezone time of %s is not right." % vm.name)
+
+        # Useless, so shutdown for cleanup
+        vm.destroy()
 
     if len(fail_info):
         raise error.TestFail(fail_info)
