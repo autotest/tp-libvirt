a2a7c3912c36e41bf0639a4e3465e0862aba79de
Yu Mingfei <yumingfei@cn.fujitsu.com>
2014-04-23 21:24:22 -0400

    libvirt.tests: Add testcases for lvm type storage migration.
    Signed-off-by: Yu Mingfei <yumingfei@cn.fujitsu.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_migrate_copy_storage.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_migrate_copy_storage.py
@@ -1,6 +1,8 @@
+import os
 import logging
+from autotest.client import lv_utils
 from autotest.client.shared import error, ssh_key
-from virttest import utils_test, remote
+from virttest import utils_test, remote, libvirt_vm
 from virttest.utils_test import libvirt as utlv
 
 
@@ -50,26 +52,89 @@ def run(test, params, env):
     Test migration with option --copy-storage-all or --copy-storage-inc.
     """
     vm = env.get_vm(params.get("main_vm"))
-    vms = [vm]
     disk_type = params.get("copy_storage_type", "file")
-    file_path, file_size = vm.get_device_size("vda")
+    primary_target = vm.get_first_disk_devices()["target"]
+    file_path, file_size = vm.get_device_size(primary_target)
     # Convert to Gib
     file_size = int(file_size) / 1073741824
 
     remote_host = params.get("remote_ip", "REMOTE.EXAMPLE")
+    local_host = params.get("local_ip", "LOCAL.EXAMPLE")
     remote_user = params.get("remote_user", "root")
     remote_passwd = params.get("remote_pwd", "PASSWORD.EXAMPLE")
-    if remote_host.count("EXAMPLE"):
-        raise error.TestNAError("Config a remote host first.")
+    if remote_host.count("EXAMPLE") or local_host.count("EXAMPLE"):
+        raise error.TestNAError("Config remote or local host first.")
     # Config ssh autologin for it
     ssh_key.setup_ssh_key(remote_host, remote_user, remote_passwd, port=22)
 
-    rdm = utils_test.RemoteDiskManager(params)
+    # Attach additional disks to vm if disk count big than 1
+    disks_count = int(params.get("added_disks_count", 1)) - 1
+    if disks_count:
+        if vm.is_alive():
+            vm.destroy()
+        new_vm_name = "%s_smtest" % vm.name
+        utlv.define_new_vm(vm.name, new_vm_name)
+        vm = libvirt_vm.VM(new_vm_name, vm.params, vm.root_dir,
+                           vm.address_cache)
+    vms = [vm]
+    if vm.is_dead():
+        vm.start()
+
     try:
-        rdm.create_image(disk_type, file_path, file_size)
+        rdm = utils_test.RemoteDiskManager(params)
+        vgname = params.get("sm_vg_name", "SMTEST")
+        added_disks_list = []
+        if disk_type == "lvm":
+            target1 = target2 = ""  # For cleanup
+            # Create volume group with iscsi
+            # For local, target is a device name
+            target1 = utlv.setup_or_cleanup_iscsi(is_setup=True, is_login=True,
+                                                  emulated_image="emulated_iscsi1")
+            lv_utils.vg_create(vgname, target1)
+            logging.debug("Created VG %s", vgname)
+            # For remote, target is real target name
+            target2 = utlv.setup_or_cleanup_iscsi(is_setup=True, is_login=False,
+                                                  emulated_image="emulated_iscsi2")
+            logging.debug("Created target: %s", target2)
+            # Login on remote host
+            remote_device = rdm.iscsi_login_setup(local_host, target2)
+            if not rdm.create_vg(vgname, remote_device):
+                import time
+                time.sleep(120)
+                raise error.TestError("Create VG %s on %s failed."
+                                      % (vgname, remote_host))
+
+        all_disks = utlv.attach_disks(vm, file_path, vgname, params)
+        # Reserve for cleanup
+        added_disks_list = all_disks.keys()
+        all_disks[file_path] = file_size
+        logging.debug("All disks need to be migrated:%s", all_disks)
+
+        for disk,size in all_disks.items():
+            if disk == file_path:
+                rdm.create_image("file", disk, size, None, None)
+            else:
+                rdm.create_image(disk_type, disk, size, vgname,
+                                 os.path.basename(disk))
         copied_migration(vms, params)
     finally:
         # Recover created vm
         if vm.is_alive():
             vm.destroy()
-        rdm.remove_path(disk_type, file_path)
+            if disks_count:
+                vm.undefine()
+        for disk in added_disks_list:
+            utlv.delete_local_disk(disk_type, disk)
+            rdm.remove_path(disk_type, disk)
+        rdm.remove_path("file", file_path)
+        if disk_type == "lvm":
+            rdm.remove_vg(vgname)
+            rdm.iscsi_login_setup(local_host, target2, is_login=False)
+            try:
+                lv_utils.vg_remove(vgname)
+            except:
+                pass    # let it go to confirm cleanup iscsi device
+            utlv.setup_or_cleanup_iscsi(is_setup=False,
+                                        emulated_image="emulated_iscsi1")
+            utlv.setup_or_cleanup_iscsi(is_setup=False,
+                                        emulated_image="emulated_iscsi2")
