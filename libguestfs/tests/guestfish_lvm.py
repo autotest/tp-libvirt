e779fac71015d93ce1835de72bd6ce51ffd21a2a
Bo Fan <bfan@redhat.com>
2014-05-22 17:28:14 +0800

    Add new file guestfish_lvm.cfg and guestfish_lvm.py for test lvm related APIs

---
+++ libguestfs/tests/guestfish_lvm.py
@@ -0,0 +1,167 @@
+from autotest.client.shared import error, utils
+from virttest import utils_test, utils_misc, data_dir
+from virttest.tests import unattended_install
+import logging
+import shutil
+import os
+import re
+
+
+def prepare_image(params):
+    """
+    (1) Create a image
+    (2) Create file system on the image
+    """
+    params["image_path"] = utils_test.libguestfs.preprocess_image(params)
+
+    if not params.get("image_path"):
+        raise error.TestFail("Image could not be created for some reason.")
+
+    gf = utils_test.libguestfs.GuestfishTools(params)
+    status, output = gf.create_fs()
+    if status is False:
+        gf.close_session()
+        raise error.TestFail(output)
+    gf.close_session()
+
+
+def test_is_lv(vm, params):
+    """
+    Test command is-lv
+    """
+    add_ref = params.get("gf_add_ref", "disk")
+    readonly = params.get("gf_add_readonly", "no")
+
+    gf = utils_test.libguestfs.GuestfishTools(params)
+    if add_ref == "disk":
+        image_path = params.get("image_path")
+        gf.add_drive_opts(image_path, readonly=readonly)
+    elif add_ref == "domain":
+        vm_name = params.get("main_vm")
+        gf.add_domain(vm_name, readonly=readonly)
+    gf.run()
+
+    if params["partition_type"] == "lvm":
+        name = gf.lvs().stdout.strip()
+        ret = gf.is_lv(name).stdout.strip()
+        if ret != "true":
+            gf.close_session()
+            raise error.TestFail("It should be a lvm device")
+    else:
+        name = gf.list_partitions().stdout.strip()
+        ret = gf.is_lv(name).stdout.strip()
+        if ret != "false":
+            gf.close_session()
+            raise error.TestFail("It should be a physical device")
+
+    gf.close_session()
+
+
+def test_lvcreate(vm, params):
+    """
+    Test command lvcreate
+    """
+    add_ref = params.get("gf_add_ref", "disk")
+    readonly = params.get("gf_add_readonly", "no")
+
+    gf = utils_test.libguestfs.GuestfishTools(params)
+    if add_ref == "disk":
+        image_path = params.get("image_path")
+        gf.add_drive_opts(image_path, readonly=readonly)
+    elif add_ref == "domain":
+        vm_name = params.get("main_vm")
+        gf.add_domain(vm_name, readonly=readonly)
+    gf.run()
+    pv_name = params.get("pv_name")
+
+    vg_name = "myvg"
+    lv_name = "mylv"
+    size = 100
+    part_name = "/dev/%s/%s" % (vg_name, lv_name)
+
+    if params["partition_type"] == "physical":
+        gf.part_init(pv_name, "msdos")
+        gf.pvcreate(pv_name)
+        gf.vgcreate(vg_name, pv_name)
+        gf.lvcreate(lv_name, vg_name, size)
+
+        result = gf.lvs().stdout.strip()
+
+        if result != part_name:
+            gf.close_session()
+            raise error.TestFail("lv name is not match")
+
+        result = gf.lvs_full().stdout.strip()
+        result = re.search("lv_name:\s+(\S+)", result).groups()[0]
+
+        if result != lv_name:
+            gf.close_session()
+            raise error.TestFail("lv name is not match")
+
+    gf.close_session()
+
+
+def test_lvm_canonical_lv_name(vm, params):
+    """
+    Test command lvm-canonical-lv-name
+    """
+    add_ref = params.get("gf_add_ref", "disk")
+    readonly = params.get("gf_add_readonly", "no")
+
+    gf = utils_test.libguestfs.GuestfishTools(params)
+    if add_ref == "disk":
+        image_path = params.get("image_path")
+        gf.add_drive_opts(image_path, readonly=readonly)
+    elif add_ref == "domain":
+        vm_name = params.get("main_vm")
+        gf.add_domain(vm_name, readonly=readonly)
+    gf.run()
+    pv_name = params.get("pv_name")
+
+    if params["partition_type"] == "lvm":
+        real_name = gf.lvs().stdout.strip()
+        vg_name, lv_name = real_name.split("/")[-2:]
+
+        test_name = "/dev/mapper/%s-%s" % (vg_name, lv_name)
+        result = gf.lvm_canonical_lv_name(test_name).stdout.strip()
+        logging.debug(result)
+
+        if result != real_name:
+            gf.close_session()
+            raise error.TestFail("Return name is uncorrect")
+
+    gf.close_session()
+
+
+def run(test, params, env):
+    """
+    Test of built-in lvm related commands in guestfish.
+
+    1) Get parameters for test
+    2) Set options for commands
+    3) Run key commands:
+       a.add disk or domain with readonly or not
+       b.launch
+       c.mount root device
+    4) Write a file to help result checking
+    5) Check result
+    """
+
+    vm_name = params.get("main_vm")
+    vm = env.get_vm(vm_name)
+
+    if vm.is_alive():
+        vm.destroy()
+
+    operation = params.get("guestfish_function")
+    testcase = globals()["test_%s" % operation]
+    partition_types = params.get("partition_types")
+    fs_types = params.get("fs_types")
+    image_formats = params.get("image_formats")
+
+    for image_format in re.findall("\w+", image_formats):
+        params["image_format"] = image_format
+        for partition_type in re.findall("\w+", partition_types):
+            params["partition_type"] = partition_type
+            prepare_image(params)
+            testcase(vm, params)
