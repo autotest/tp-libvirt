c7a84c6be3c1f1f32bfd16832cb49833b461e4e0
Yu Mingfei <yumingfei@cn.fujitsu.com>
2014-06-06 17:33:34 +0800

    libvirt.tests: Add filter for migrate speed with bz1083483.
    Signed-off-by: Yu Mingfei <yumingfei@cn.fujitsu.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_migrate_set_get_speed.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_migrate_set_get_speed.py
@@ -1,11 +1,23 @@
 import logging
 from autotest.client.shared import error
 from virttest import virsh
+from provider import libvirt_version
 
 UINT32_MAX = (1 << 32) - 1
 INT64_MAX = (1 << 63) - 1
 UINT64_MAX = (1 << 64) - 1
-DEFAULT = INT64_MAX / (1024 * 1024)
+
+# The virsh migrate-setspeed indicates units are in MiB
+# and not bytes, so adjust our values. This was not checked
+# until libvirt 1.2.4 by commit id's 'c4206d7c' and 'dff3ad00'
+# although it's been true since the code was initially added.
+# This test thus must use proper values.  Additionally, code
+# points out that max is an INT64, which is not called out in
+# the man page (yet).
+UINT32_MiB = UINT32_MAX / (1024 * 1024)
+INT64_MiB = INT64_MAX / (1024 * 1024)
+UINT64_MiB = UINT64_MAX / (1024 * 1024)
+DEFAULT = INT64_MiB
 
 
 def run(test, params, env):
@@ -28,20 +40,26 @@ def run(test, params, env):
     status_error = "yes" == params.get("status_error", "yes")
     virsh_dargs = {'debug': True}
 
+    bz1083483 = False
     if bandwidth == "zero":
         expected_value = 0
     elif bandwidth == "one":
         expected_value = 1
+    elif bandwidth == "negative":
+        expected_value = -1
+        bz1083483 = True
     elif bandwidth == "default":
         expected_value = DEFAULT
     elif bandwidth == "UINT32_MAX":
-        expected_value = UINT32_MAX
+        expected_value = UINT32_MiB
     elif bandwidth == "INT64_MAX":
-        expected_value = INT64_MAX
+        expected_value = INT64_MiB
     elif bandwidth == "UINT64_MAX":
-        expected_value = UINT64_MAX
+        expected_value = UINT64_MiB
+        bz1083483 = True
     elif bandwidth == "INVALID_VALUE":
-        expected_value = UINT64_MAX + 1
+        expected_value = INT64_MiB + 1
+        bz1083483 = True
     else:
         expected_value = bandwidth
 
@@ -57,7 +75,12 @@ def run(test, params, env):
         # Check status_error
         if status_error:
             if status == 0 or err == "":
-                raise error.TestFail("Expect fail, but run successfully!")
+                # Without code for bz1083483 applied, this will succeed
+                # when it shouldn't be succeeding.
+                if bz1083483 and not libvirt_version.version_compare(1, 2, 4):
+                    raise error.TestNAError("bz1083483 should result in fail")
+                else:
+                    raise error.TestFail("Expect fail, but run successfully!")
 
             # no need to perform getspeed if status_error is true
             return
