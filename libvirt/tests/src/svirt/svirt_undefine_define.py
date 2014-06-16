9df81d9be2ea0e9b02c5e52d260ea0bd12962ce9
Dongsheng Yang <yangds.fnst@cn.fujitsu.com>
2014-06-16 19:29:47 +0900

    svirt: fix some bug in svirt.
    Signed-off-by: Dongsheng Yang <yangds.fnst@cn.fujitsu.com>

---
+++ libvirt/tests/src/svirt/svirt_undefine_define.py
@@ -0,0 +1,75 @@
+"""
+svirt guest_undefine_define test.
+"""
+import os
+
+from autotest.client.shared import error
+from virttest import utils_selinux, virsh, data_dir
+from virttest.libvirt_xml.vm_xml import VMXML
+
+
+def run(test, params, env):
+    """
+    Test svirt in adding disk to VM.
+
+    (1).Init variables for test.
+    (2).Label the VM and disk with proper label.
+    (3).Start VM and check the context.
+    (4).Destroy VM and check the context.
+    """
+    # Get general variables.
+    status_error = ('yes' == params.get("status_error", 'no'))
+    host_sestatus = params.get(
+        "svirt_undefine_define_host_selinux", "enforcing")
+    # Get variables about seclabel for VM.
+    sec_type = params.get("svirt_undefine_define_vm_sec_type", "dynamic")
+    sec_model = params.get("svirt_undefine_define_vm_sec_model", "selinux")
+    sec_label = params.get("svirt_undefine_define_vm_sec_label", None)
+    sec_relabel = params.get("svirt_undefine_define_vm_sec_relabel", "yes")
+    sec_dict = {'type': sec_type, 'model': sec_model, 'label': sec_label,
+                'relabel': sec_relabel}
+    poweroff_with_destroy = ("destroy" == params.get(
+        "svirt_undefine_define_vm_poweroff", "destroy"))
+    # Get variables about VM and get a VM object and VMXML instance.
+    vm_name = params.get("main_vm")
+    vm = env.get_vm(vm_name)
+    vmxml = VMXML.new_from_inactive_dumpxml(vm_name)
+    backup_xml = vmxml.copy()
+
+    # Get varialbles about image.
+    img_label = params.get('svirt_undefine_define_disk_label')
+    # Label the disks of VM with img_label.
+    disks = vm.get_disk_devices()
+    backup_labels_of_disks = {}
+    for disk in disks.values():
+        disk_path = disk['source']
+        backup_labels_of_disks[disk_path] = utils_selinux.get_context_of_file(
+            filename=disk_path)
+        utils_selinux.set_context_of_file(filename=disk_path,
+                                          context=img_label)
+    # Set selinux of host.
+    backup_sestatus = utils_selinux.get_status()
+    utils_selinux.set_status(host_sestatus)
+    # Set the context of the VM.
+    vmxml.set_seclabel(sec_dict)
+    vmxml.sync()
+
+    try:
+        xml_file = (os.path.join(data_dir.get_tmp_dir(), "vmxml"))
+        if vm.is_alive():
+            vm.destroy()
+        virsh.dumpxml(vm.name, to_file=xml_file)
+        cmd_result = virsh.undefine(vm.name)
+        if cmd_result.exit_status:
+            raise error.TestFail("Failed to undefine vm."
+                                 "Detail: %s" % cmd_result)
+        cmd_result = virsh.define(xml_file)
+        if cmd_result.exit_status:
+            raise error.TestFail("Failed to define vm."
+                                 "Detail: %s" % cmd_result)
+    finally:
+        # clean up
+        for path, label in backup_labels_of_disks.items():
+            utils_selinux.set_context_of_file(filename=path, context=label)
+        backup_xml.sync()
+        utils_selinux.set_status(backup_sestatus)
---
+++ libvirt/tests/src/svirt/svirt_virt_clone.py
@@ -0,0 +1,69 @@
+"""
+svirt virt-clone test.
+"""
+from autotest.client import utils
+from autotest.client.shared import error
+from virttest import utils_selinux, virt_vm, utils_misc, virsh
+from virttest.libvirt_xml.vm_xml import VMXML
+
+
+def run(test, params, env):
+    """
+    Test svirt in virt-clone.
+    """
+    VIRT_CLONE = None
+    try:
+        VIRT_CLONE = utils_misc.find_command("virt-clone")
+    except ValueError:
+        raise error.TestNAError("No virt-clone command found.")
+
+    # Get general variables.
+    status_error = ('yes' == params.get("status_error", 'no'))
+    host_sestatus = params.get("svirt_virt_clone_host_selinux", "enforcing")
+    # Get variables about seclabel for VM.
+    sec_type = params.get("svirt_virt_clone_vm_sec_type", "dynamic")
+    sec_model = params.get("svirt_virt_clone_vm_sec_model", "selinux")
+    sec_label = params.get("svirt_virt_clone_vm_sec_label", None)
+    sec_relabel = params.get("svirt_virt_clone_vm_sec_relabel", "yes")
+    sec_dict = {'type': sec_type, 'model': sec_model, 'label': sec_label,
+                'relabel': sec_relabel}
+    # Get variables about VM and get a VM object and VMXML instance.
+    vm_name = params.get("main_vm")
+    vm = env.get_vm(vm_name)
+    vmxml = VMXML.new_from_inactive_dumpxml(vm_name)
+    backup_xml = vmxml.copy()
+
+    # Get varialbles about image.
+    img_label = params.get('svirt_virt_clone_disk_label')
+    # Label the disks of VM with img_label.
+    disks = vm.get_disk_devices()
+    backup_labels_of_disks = {}
+    for disk in disks.values():
+        disk_path = disk['source']
+        backup_labels_of_disks[disk_path] = utils_selinux.get_context_of_file(
+            filename=disk_path)
+        utils_selinux.set_context_of_file(filename=disk_path,
+                                          context=img_label)
+    # Set selinux of host.
+    backup_sestatus = utils_selinux.get_status()
+    utils_selinux.set_status(host_sestatus)
+    # Set the context of the VM.
+    vmxml.set_seclabel(sec_dict)
+    vmxml.sync()
+
+    clone_name = ("%s-clone" % vm.name)
+    try:
+        cmd = ("%s --original %s --name %s --auto-clone" %
+               (VIRT_CLONE, vm.name, clone_name))
+        cmd_result = utils.run(cmd, ignore_status=True)
+        if cmd_result.exit_status:
+            raise error.TestFail("Failed to execute virt-clone command."
+                                 "Detail: %s." % cmd_result)
+    finally:
+        # clean up
+        for path, label in backup_labels_of_disks.items():
+            utils_selinux.set_context_of_file(filename=path, context=label)
+        backup_xml.sync()
+        utils_selinux.set_status(backup_sestatus)
+        if not virsh.domstate(clone_name).exit_status:
+            virsh.remove_domain(clone_name)
