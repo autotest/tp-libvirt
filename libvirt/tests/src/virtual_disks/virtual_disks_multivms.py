3f24c17d2bd81df8d2f8e8675c82893f183cfe57
rbian <rbian@redhat.com>
2014-06-20 16:34:32 +0800

    Test shableable block disk on 2 domains.

--- libvirt/tests/src/virtual_disks/virtual_disks_multivms.py
+++ libvirt/tests/src/virtual_disks/virtual_disks_multivms.py
@@ -81,7 +81,7 @@ def run(test, params, env):
         # Redefine domain
         vmxml.sync()
 
-    def get_vm_disk_xml(dev_type, dev_name, sgio="", share="", options=""):
+    def get_vm_disk_xml(dev_type, dev_name, **options):
         """
         Create a disk xml object and return it.
 
@@ -93,31 +93,35 @@ def run(test, params, env):
         """
         # Create disk xml
         disk_xml = Disk(type_name=dev_type)
-        if sgio != "":
-            disk_xml.sgio = sgio
+        disk_xml.device = "disk"
+        if options.has_key("sgio") and options["sgio"] != "":
+            disk_xml.sgio = options["sgio"]
             disk_xml.device = "lun"
             disk_xml.rawio = "no"
+
+        if dev_type == "block":
             disk_attr = "dev"
-            disk_xml.target = {'dev': 'sda', 'bus': 'scsi'}
         else:
-            disk_xml.device = "disk"
             disk_attr = "file"
-            disk_xml.target = {'dev': 'vdb', 'bus': 'virtio'}
+
+        disk_xml.target = {'dev': options["target"],
+                           'bus': options["bus"]}
         disk_xml.source = disk_xml.new_disk_source(
             **{'attrs': {disk_attr: dev_name}})
 
         # Add driver options from parameters.
-        driver_dict = {"name": "qemu", "type": "raw"}
-        if options != "":
-            for driver_option in options.split(','):
+        driver_dict = {"name": "qemu"}
+        if options.has_key("driver"):
+            for driver_option in options["driver"].split(','):
                 if driver_option != "":
                     d = driver_option.split('=')
                     logging.debug("disk driver option: %s=%s", d[0], d[1])
                     driver_dict.update({d[0].strip(): d[1].strip()})
 
         disk_xml.driver = driver_dict
-        if share == "shareable":
-            disk_xml.share = True
+        if options.has_key("share"):
+            if options["share"] == "shareable":
+                disk_xml.share = True
 
         return disk_xml
 
@@ -130,24 +134,46 @@ def run(test, params, env):
     vms_share = params.get("virt_disk_vms_share", "").split()
     disk_bus = params.get("virt_disk_bus", "virtio")
     disk_target = params.get("virt_disk_target", "vdb")
-    disk_type = params.get("virt_disk_tyep", "file")
+    disk_type = params.get("virt_disk_type", "file")
+    disk_format = params.get("virt_disk_format", "")
     scsi_options = params.get("scsi_options", "")
     disk_driver_options = params.get("disk_driver_options", "")
     hotplug = "yes" == params.get("virt_disk_vms_hotplug", "no")
     status_error = params.get("status_error").split()
-    test_error_policy = "yes" == params.get("virt_disk_test_error_policy", "no")
+    test_error_policy = "yes" == params.get("virt_disk_test_error_policy",
+                                            "no")
+    test_shareable = "yes" == params.get("virt_disk_test_shareable", "no")
     disk_source_path = test.virtdir
 
     disks = []
-    if disk_bus == "scsi":
+    if disk_format == "scsi":
         disk_source = get_scsi_disk(scsi_options)
         if not disk_source:
             raise error.TestNAError("Get scsi disk failed.")
         disks.append({"format": "scsi", "source": disk_source})
 
-    elif disk_bus == "virtio":
-        disk_dev, disk_source = create_disk_img("test", disk_source_path, "raw")
-        disks.append({"format": "raw", "disk_dev": disk_dev, "source": disk_source})
+    elif disk_format == "iscsi":
+        # Create iscsi device if neened.
+        disk_dev = qemu_storage.Iscsidev(params, disk_source_path,
+                                         "iscsi")
+        disk_source = disk_dev.setup()
+        logging.debug("iscsi dev name: %s", disk_source)
+        # Format the disk and make the file system.
+        open("/tmp/fdisk-cmd", "w").write("n\np\n\n\n\nw\n")
+        output = utils.run("fdisk %s < /tmp/fdisk-cmd"
+                           % disk_source).stdout.strip()
+        logging.debug("fdisk output: %s", output)
+        output = utils.run("mkfs.ext3 %s1"
+                           % disk_source).stdout.strip()
+        logging.debug("mkfs output: %s", output)
+        disk_source += "1"
+        disks.append({"format": disk_format, "disk_dev": disk_dev,
+                      "source": disk_source})
+    elif disk_format in ["raw", "qcow2"]:
+        disk_dev, disk_source = create_disk_img("test", disk_source_path,
+                                                disk_format)
+        disks.append({"format": disk_format, "disk_dev": disk_dev,
+                      "source": disk_source})
 
     # Backup xml files, compose the new domain xml
     vms_list = []
@@ -167,13 +193,14 @@ def run(test, params, env):
             set_vm_controller_xml(vmxml)
         disk_sgio = ""
         if len(vms_sgio) > i:
-            sgio = vms_sgio[i]
-        disk_sharable = ""
+            disk_sgio = vms_sgio[i]
+        shareable = ""
         if len(vms_share) > i:
-            share = vms_share[i]
+            shareable = vms_share[i]
         disk_xml = get_vm_disk_xml(disk_type, disk_source,
-                                   disk_sgio, disk_sharable,
-                                   disk_driver_options)
+                                   sgio=disk_sgio, share=shareable,
+                                   target=disk_target, bus=disk_bus,
+                                   driver=disk_driver_options)
         if not hotplug:
             # If we are not testing hotplug,
             # add disks to domain xml and sync.
@@ -190,6 +217,10 @@ def run(test, params, env):
             try:
                 # Try to start the domain.
                 vms_list[i]['vm'].start()
+                # Check if VM is started as expected.
+                if not vms_list[i]['status']:
+                    raise error.TestFail('VM started unexpectedly.')
+
                 session = vms_list[i]['vm'].wait_for_login()
                 # if we are testing hotplug, it need to start domain and
                 # then run virsh attach-device command.
@@ -205,11 +236,8 @@ def run(test, params, env):
                         raise error.TestFail('Failed to hotplug disk device')
                     elif 0 == result and not vms_list[i]['status']:
                         raise error.TestFail('Hotplug disk device unexpectedly.')
-                # Check if VM is started as expected.
-                if not vms_list[i]['status']:
-                    raise error.TestFail('VM started unexpectedly.')
 
-                # Check disk error_policy option in VM.
+                # Check disk error_policy option in VMs.
                 if test_error_policy:
                     error_policy = vms_list[i]['disk'].driver["error_policy"]
                     if i == 0:
@@ -231,7 +259,6 @@ def run(test, params, env):
                             session.close()
                             raise error.TestFail("Test error_policy: "
                                                  "failed to mount disk")
-                        session.close()
                     if i == 1:
                         try:
                             session0 = vms_list[0]['vm'].wait_for_login(timeout=10)
@@ -245,7 +272,7 @@ def run(test, params, env):
                             s, o = session0.cmd_status_output(cmd)
                             logging.debug("session in vm0 exit %s; output: %s", s, o)
                             if error_policy == "report":
-                                if 0 != s:
+                                if s:
                                     raise error.TestFail("Test error_policy %s: cann't report"
                                                          " error" % error_policy)
                             elif error_policy == "ignore":
@@ -263,13 +290,39 @@ def run(test, params, env):
                                 raise error.TestFail("Test error_policy %s: login failed"
                                                      % error_policy)
 
+                if test_shareable:
+                    if i == 1:
+                        try:
+                            test_str = "teststring"
+                            # Try to write on vm0.
+                            session0 = vms_list[0]['vm'].wait_for_login(timeout=10)
+                            cmd = ("fdisk -l /dev/%s && mount /dev/%s /mnt && echo '%s' "
+                                   "> /mnt/test && umount /mnt"
+                                   % (disk_target, disk_target, test_str))
+                            s, o = session0.cmd_status_output(cmd)
+                            logging.debug("session in vm0 exit %s; output: %s", s, o)
+                            if s:
+                                raise error.TestFail("Test disk shareable on VM0 failed")
+                            session0.close()
+                            # Try to read on vm1.
+                            cmd = ("fdisk -l /dev/%s && mount /dev/%s /mnt && grep %s"
+                                   " /mnt/test && umount /mnt"
+                                   % (disk_target, disk_target, test_str))
+                            s, o = session.cmd_status_output(cmd)
+                            logging.debug("session in vm1 exit %s; output: %s", s, o)
+                            if s:
+                                raise error.TestFail("Test disk shareable on VM1 failed")
+                        except (remote.LoginError, virt_vm.VMError, aexpect.ShellError), e:
+                            logging.error(str(e))
+                            raise error.TestFail("Test disk shareable: login failed")
+                session.close()
             except virt_vm.VMStartError:
                 if vms_list[i]['status']:
                     raise error.TestFail('VM Failed to start'
                                          ' for some reason!')
     finally:
         # Recover VMs.
-        for i in range(len(vm_names)):
+        for i in range(len(vms_list)):
             if vms_list[i]['vm'].is_alive():
                 vms_list[i]['vm'].destroy(gracefully=False)
             logging.info("Restoring vm...")
@@ -279,7 +332,10 @@ def run(test, params, env):
         # Remove disks.
         for img in disks:
             if img.has_key("disk_dev"):
-                img["disk_dev"].remove()
+                if img["format"] in ["iscsi"]:
+                    img["disk_dev"].cleanup()
+                else:
+                    img["disk_dev"].remove()
             else:
                 if img["format"] == "scsi":
                     utils.unload_module("scsi_debug")
