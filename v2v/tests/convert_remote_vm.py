2731c1292fbdc730f0cf7d0641ea17acd2f2895d
Yu Mingfei <yumingfei@cn.fujitsu.com>
2014-07-08 12:16:34 +0900

    v2v.tests: Use PoolVolumeTest to create pool for converting remote vm.
    Signed-off-by: Yu Mingfei <yumingfei@cn.fujitsu.com>

--- v2v/tests/convert_remote_vm.py
+++ v2v/tests/convert_remote_vm.py
@@ -5,6 +5,7 @@ from autotest.client import lv_utils
 from autotest.client.shared import ssh_key, error
 from virttest import utils_v2v, libvirt_storage, libvirt_vm, virsh
 from virttest import virt_vm, remote, data_dir
+from virttest.utils_test import libvirt as utlv
 
 
 def create_dir_pool(spool, pool_name, target_path):
@@ -20,65 +21,6 @@ def create_dir_pool(spool, pool_name, target_path):
         return False
 
     if not spool.build_pool(pool_name):
-        return False
-
-    if not spool.start_pool(pool_name):
-        return False
-
-    if not spool.set_pool_autostart(pool_name):
-        return False
-    return True
-
-
-def create_partition_pool(spool, pool_name, block_device,
-                          target_path="/dev/v2v_test"):
-    """
-    Create a persistent partition pool.
-    """
-    # Check pool before creating
-    if spool.pool_exists(pool_name):
-        logging.debug("Pool '%s' already exists.", pool_name)
-        return False
-
-    if not spool.define_fs_pool(pool_name, block_device,
-                                target_path=target_path):
-        return False
-
-    if not spool.build_pool(pool_name):
-        return False
-
-    if not spool.start_pool(pool_name):
-        return False
-
-    if not spool.set_pool_autostart(pool_name):
-        return False
-    return True
-
-
-def create_lvm_pool(spool, pool_name, block_device, vg_name="vg_v2v",
-                    target_path="/dev/vg_v2v"):
-    """
-    Create a persistent lvm pool.
-    """
-    # Check pool before creating
-    if spool.pool_exists(pool_name):
-        logging.debug("Pool '%s' already exists.", pool_name)
-        return False
-
-    if not spool.define_lvm_pool(pool_name, block_device, vg_name=vg_name,
-                                 target_path=target_path):
-        return False
-
-    vgroups = lv_utils.vg_list()
-    if vg_name not in vgroups.keys() and not spool.build_pool(pool_name):
-        return False
-
-    # To verify if volume group has been set.
-    try:
-        lv_utils.lv_create(vg_name, "v2v_sample", "10M", True)
-        lv_utils.lv_remove(vg_name, "v2v_sample")
-    except (error.TestError, error.CmdError), detail:
-        logging.error("Check volume group failed:\n%s", str(detail))
         return False
 
     if not spool.start_pool(pool_name):
@@ -144,22 +86,12 @@ def run(test, params, env):
     pool_type = params.get("pool_type", "dir")
     pool_name = params.get("pool_name", "v2v_test")
     target_path = params.get("target_path", "pool_path")
-    block_device = params.get("block_device", "/dev/BLOCK/EXAMPLE")
-    vg_name = params.get("volume_group_name", "vg_v2v")
-
-    # Confirm parameters have been set correctly.
-    if (pool_type in ['partition', 'lvm'] and
-            re.search("EXAMPLE", block_device)):
-        raise error.TestNAError("Please set correct block device.")
+    emulated_img = params.get("emulated_image_path", "v2v_emulated.img")
+    emulated_size = params.get("emulated_image_size", "10G")
 
     # If target_path is not an abs path, join it to data_dir.tmpdir
     if os.path.dirname(target_path) is "":
         target_path = os.path.join(data_dir.get_tmp_dir(), target_path)
-
-    # dir pool need an exist path
-    if pool_type == "dir":
-        if not os.path.exists(target_path):
-            os.mkdir(target_path)
 
     # V2V parameters
     input = params.get("input_method")
@@ -201,24 +133,16 @@ def run(test, params, env):
         # Put remote vm's disk into a directory storage pool
         prepare_remote_sp(rsp, remote_vm, pool_name)
 
+    # Prepare local libvirt storage pool
+    pvt = utlv.PoolVolumeTest(test, params)
+
     # Local storage pool's instance
     lsp = libvirt_storage.StoragePool()
     try:
         # Create storage pool for test
-        if pool_type == "dir":
-            if not create_dir_pool(lsp, pool_name, target_path):
-                raise error.TestFail("Prepare directory storage pool for "
-                                     "virt-v2v failed.")
-        elif pool_type == "partition":
-            if not create_partition_pool(lsp, pool_name, block_device,
-                                         target_path):
-                raise error.TestFail("Prepare partition storage pool for "
-                                     "virt-v2v failed.")
-        elif pool_type == "lvm":
-            if not create_lvm_pool(lsp, pool_name, block_device, vg_name,
-                                   target_path):
-                raise error.TestFail("Preapre lvm storage pool for "
-                                     "virt-v2v failed.")
+        pvt.pre_pool(pool_name, pool_type, target_path, emulated_img,
+                     emulated_size)
+        logging.debug(lsp.pool_info(pool_name))
 
         # Maintain a single params for v2v to avoid duplicate parameters
         v2v_params = {"hostname": remote_hostname, "username": username,
@@ -257,6 +181,11 @@ def run(test, params, env):
             raise error.TestFail(error_info)
     finally:
         cleanup_vm(vm_name)
-        lsp.delete_pool(pool_name)
+        try:
+            # Create pool may be not in autostart, allowing to raise out
+            pvt.cleanup_pool(pool_name, pool_type, target_path, emulated_img)
+        except error.TestFail, detail:   # Make sure cleanup will be finished
+            logging.warn(detail)
         if remote_hypervisor != "esx":
             rsp.delete_pool(pool_name)
+        rvirsh.close_session()
