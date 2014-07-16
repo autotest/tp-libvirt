88a78f9b7e9503a651ed78ba6be1dd14398ec58e
Dongsheng Yang <yangds.fnst@cn.fujitsu.com>
2014-07-16 15:35:17 +0900

    Add test for virt-what command.
    Signed-off-by: Dongsheng Yang <yangds.fnst@cn.fujitsu.com>

---
+++ libvirt/tests/src/virt_cmd/virt_what.py
@@ -0,0 +1,32 @@
+from autotest.client.shared import error
+
+
+def run(test, params, env):
+    """
+    Test for virt-what, it should be executed in guest
+    and tell user which typervisor it is using.
+
+    (1). Login guest & execute virt-waht command.
+    (2). Check the result, lxc or kvm.
+    """
+    vm_name = params.get("main_vm", "virt-tests-vm1")
+    vm = env.get_vm(vm_name)
+    expect_output = params.get("fact")
+
+    if not vm.is_alive():
+        vm.start()
+    session = vm.wait_for_login()
+
+    # Check whether the command of virt-what is available
+    # on guest.
+    status = session.cmd_status("which virt-what")
+    if status:
+        # Skip if virt-what is not available on guest.
+        raise error.TestNAError("No virt-what command in guest.")
+
+    # Execute virt-what on guest.
+    output = session.cmd_output("virt-what").strip()
+
+    if not output == expect_output:
+        raise error.TestFail("Output of virt-what in guest is <%s>,"
+                             "but we expect <%s>.\n" % (output, expect_output))
