e7a640a88eab3ccd0471011acf9c40d30510fc24
Hao Liu <hliu@redhat.com>
2014-08-13 16:44:31 +0800

    Add test for set_process_name in qemu.conf
    Test set_process_name parameter in qemu.conf.
    
    1) Change set_process_name in qemu.conf;
    2) Restart libvirt daemon;
    3) Check if libvirtd successfully started;
    4) Check if qemu command line changed accordingly;
    
    Signed-off-by: Hao Liu <hliu@redhat.com>

---
+++ libvirt/tests/src/conf_file/qemu_conf/set_process_name.py
@@ -0,0 +1,89 @@
+import re
+import time
+import logging
+from virttest import utils_config
+from virttest import utils_libvirtd
+from autotest.client import utils
+from autotest.client.shared import error
+
+
+def run(test, params, env):
+    """
+    Test set_process_name parameter in qemu.conf.
+
+    1) Change set_process_name in qemu.conf;
+    2) Restart libvirt daemon;
+    3) Check if libvirtd successfully started;
+    4) Check if qemu command line changed accordingly;
+    """
+    def get_qemu_command_name_option(vm):
+        """
+        Get the name option of qemu command line of a libvirt VM.
+
+        :param vm: A libvirt_vm.VM class instance.
+        :return :  A string containing '-name' option of VM's qemu command
+                   line or None if error.
+        """
+        if vm.is_dead():
+            vm.start()
+
+        # Get qemu command line.
+        pid = vm.get_pid()
+        res = utils.run("ps -p %s -o cmd h" % pid)
+
+        if res.exit_status == 0:
+            match = re.search(r'-name\s*(\S*)', res.stdout)
+            if match:
+                return match.groups()[0]
+
+    vm_name = params.get("main_vm", "virt-tests-vm1")
+    expected_result = params.get("expected_result", "name_not_set")
+    set_process_name = params.get("set_process_name", "not_set")
+    vm = env.get_vm(vm_name)
+
+    # Get old qemu -name option.
+    orig_qemu_name = get_qemu_command_name_option(vm)
+    logging.debug('Original "-name" option of qemu command is '
+                  '"%s".' % orig_qemu_name)
+
+    config = utils_config.LibvirtQemuConfig()
+    libvirtd = utils_libvirtd.Libvirtd()
+    try:
+        if set_process_name == 'not_set':
+            del config.set_process_name
+        else:
+            config.set_process_name = set_process_name
+
+        # Restart libvirtd to make change valid.
+        if not libvirtd.restart():
+            if expected_result != 'unbootable':
+                raise error.TestFail('Libvirtd is expected to be started '
+                                     'with set_process_name = '
+                                     '%s' % set_process_name)
+            return
+        if expected_result == 'unbootable':
+            raise error.TestFail('Libvirtd is not expected to be started '
+                                 'with set_process_name = '
+                                 '%s' % set_process_name)
+
+        # Restart VM to create a new qemu command line.
+        if vm.is_alive():
+            vm.destroy()
+        vm.start()
+
+        # Get new qemu -name option.
+        new_qemu_name = get_qemu_command_name_option(vm)
+        logging.debug('New "-name" option of qemu command is '
+                      '"%s"' % new_qemu_name)
+
+        if ',process=qemu:%s' % vm_name in new_qemu_name:
+            if expected_result == 'name_not_set':
+                raise error.TestFail('Qemu name is not expected to set, '
+                                     'but %s found' % new_qemu_name)
+        else:
+            if expected_result == 'name_set':
+                raise error.TestFail('Qemu name is expected to set, '
+                                     'but %s found' % new_qemu_name)
+    finally:
+        config.restore()
+        libvirtd.restart()
