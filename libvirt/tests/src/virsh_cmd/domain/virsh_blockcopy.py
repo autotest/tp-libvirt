682323bc2b60e375625359e4a8364b3c7595bd56
Wayne Sun <gsun@redhat.com>
2014-11-24 14:51:36 +0800

    blockcopy: Raise error when blockcopy timeout
    As blockcopy will got hang due to bug 1134294, so set the timeout
    for virsh command, also check the error in libvirtd log after command
    timeout.
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_blockcopy.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_blockcopy.py
@@ -4,10 +4,12 @@ import time
 import re
 from autotest.client.shared import error
 from virttest import utils_libvirtd
+from virttest import utils_config
 from virttest import virsh
 from virttest import qemu_storage
 from virttest import data_dir
 from virttest import aexpect
+from virttest import utils_misc
 from virttest.libvirt_xml import vm_xml
 from virttest.utils_test import libvirt as utl
 from provider import libvirt_version
@@ -134,6 +136,22 @@ def finish_job(vm_name, target, timeout):
         raise JobTimeout(timeout)
 
 
+def chk_libvirtd_log(file_path, pattern, log_type):
+    """
+    Check whether libvirtd log contains specific info
+    :param file_path: libvirtd log path
+    :param pattern: string, pattern to check
+    :param log_type: string, debug, error or warning
+
+    :return: True or False
+    """
+    with open(file_path) as f:
+        for line in f:
+            if pattern in line and log_type in line:
+                return True
+    return False
+
+
 def run(test, params, env):
     """
     Test command: virsh blockcopy.
@@ -172,6 +190,7 @@ def run(test, params, env):
     active_sanp = "yes" == params.get("active_sanp", "no")
     active_save = "yes" == params.get("active_save", "no")
     check_state_lock = "yes" == params.get("check_state_lock", "no")
+    timeout = int(params.get("timeout", 1200))
     rerun_flag = 0
 
     original_xml = vm.backup_xml()
@@ -233,6 +252,19 @@ def run(test, params, env):
             raise error.TestNAError("API acl test not supported in current"
                                     + " libvirt version.")
 
+    extra_dict = {'uri': uri, 'unprivileged_user': unprivileged_user,
+                  'debug': True, 'ignore_status': True, 'timeout': timeout}
+
+    libvirtd_utl = utils_libvirtd.Libvirtd()
+    libvirtd_conf = utils_config.LibvirtdConfig()
+    libvirtd_conf["log_filters"] = '"3:json 1:libvirt 1:qemu"'
+    libvirtd_log_path = os.path.join(test.tmpdir, "libvirtd.log")
+    libvirtd_conf["log_outputs"] = '"1:file:%s"' % libvirtd_log_path
+    logging.debug("the libvirtd config file content is:\n %s" %
+                  libvirtd_conf)
+    libvirtd_utl.restart()
+    bug_log = "Hit on bug: https://bugzilla.redhat.com/show_bug.cgi?id=1134294"
+
     def check_format(dest_path, dest_extension, expect):
         """
         Check the image format
@@ -251,40 +283,46 @@ def run(test, params, env):
         else:
             raise error.TestFail("%s format is not %s." % (dest_path, expect))
 
-    # Run virsh command
-    try:
-        if rerun_flag == 1:
-            options1 = "--wait --raw --finish --verbose"
-            cmd_result = virsh.blockcopy(vm_name, target, dest_path, options1,
-                                         unprivileged_user=unprivileged_user,
-                                         uri=uri,
-                                         ignore_status=True, debug=True)
-            status = cmd_result.exit_status
-            if status != 0:
-                raise error.TestFail("Run blockcopy command fail.")
-            elif not os.path.exists(dest_path):
-                raise error.TestFail("Cannot find the created copy.")
+    def blockcopy_chk():
+        """
+        Raise TestFail when blockcopy hang with state change lock
+        """
+        err_pattern = "Timed out during operation: cannot acquire"
+        err_pattern += " state change lock"
+        ret = chk_libvirtd_log(libvirtd_log_path, err_pattern, "error")
+        if ret:
+            raise error.TestFail("blockcopy command hang, %s" % bug_log)
 
-        cmd_result = virsh.blockcopy(vm_name, target, dest_path, options,
-                                     unprivileged_user=unprivileged_user,
-                                     uri=uri,
-                                     ignore_status=True, debug=True)
-        status = cmd_result.exit_status
-    except Exception, detail:
-        logging.error(detail)
-
-    if not utils_libvirtd.libvirtd_is_running():
-        raise error.TestFail("Libvirtd service is dead.")
-    # Check_result
     snap_path = ''
     save_path = ''
     try:
         try:
+            # Run blockcopy command
+            if rerun_flag == 1:
+                options1 = "--wait --raw --finish --verbose"
+                cmd_result = virsh.blockcopy(vm_name, target,
+                                             dest_path, options1,
+                                             **extra_dict)
+                status = cmd_result.exit_status
+                if status != 0:
+                    raise error.TestFail("Run blockcopy command fail.")
+                elif not os.path.exists(dest_path):
+                    raise error.TestFail("Cannot find the created copy.")
+
+            cmd_result = virsh.blockcopy(vm_name, target, dest_path,
+                                         options,
+                                         **extra_dict)
+            status = cmd_result.exit_status
+
+            if not libvirtd_utl.is_running():
+                raise error.TestFail("Libvirtd service is dead.")
+
             if not status_error:
                 if status == 0:
                     check_xml(vm_name, target, dest_path, options)
                     if options.count("--bandwidth"):
-                        utl.check_blockjob(vm_name, target, "bandwidth", bandwidth)
+                        utl.check_blockjob(vm_name, target,
+                                           "bandwidth", bandwidth)
                         if check_state_lock:
                             # Run blockjob pivot in subprocess as it will hang
                             # for a while, run blockjob info again to check
@@ -295,16 +333,11 @@ def run(test, params, env):
                             ret = virsh.blockjob(vm_name, target, "--info")
                             err_info = "cannot acquire state change lock"
                             if err_info in ret.stderr:
-                                err_log = "Hit on bug: https://bugzilla.redhat"
-                                err_log += ".com/show_bug.cgi?id=1134294"
-                                logging.error(err_log)
-                                # restart libvirtd as the domain state will not
-                                # change if hit on the bug
-                                libvirtd_utl = utils_libvirtd.Libvirtd()
-                                libvirtd_utl.restart()
+                                logging.error(bug_log)
                             utl.check_exit_status(ret, status_error)
                             session.close()
-                    if options.count("--pivot") + options.count("--finish") == 0:
+                    val = options.count("--pivot") + options.count("--finish")
+                    if val == 0:
                         finish_job(vm_name, target, default_timeout)
                     if options.count("--raw"):
                         check_format(dest_path, dest_extension, dest_format)
@@ -325,10 +358,8 @@ def run(test, params, env):
                 else:
                     err_msg = "internal error: unable to execute QEMU command"
                     err_msg += " 'block-job-complete'"
-                    err_log = "Hit on bug: https://bugzilla.redhat.com/show_"
-                    err_log += "bug.cgi?id=1134294"
                     if err_msg in cmd_result.stderr:
-                        logging.error(err_log)
+                        logging.error(bug_log)
                     raise error.TestFail(cmd_result.stderr)
             else:
                 if status:
@@ -343,13 +374,21 @@ def run(test, params, env):
                             re.search("Copy aborted", cmd_result.stdout):
                         logging.debug("Found success a timed out block copy")
                     else:
-                        raise error.TestFail("Expect fail, but run successfully.")
-        except JobTimeout, excpt:
+                        raise error.TestFail("Expect fail, but run "
+                                             "successfully.")
+        except (JobTimeout, Exception), excpt:
+            blockcopy_chk()
             if not status_error:
                 raise error.TestFail("Run command failed: %s" % excpt)
     finally:
+        # Restore libvirtd conf and restart libvirtd
+        libvirtd_conf.restore()
+        libvirtd_utl.restart()
+        if libvirtd_log_path and os.path.exists(libvirtd_log_path):
+            os.unlink(libvirtd_log_path)
+
         if vm.is_alive():
-            vm.destroy()
+            vm.destroy(gracefully=False)
             virsh.define(original_xml)
 
         if disk_src_protocol == 'netfs':
