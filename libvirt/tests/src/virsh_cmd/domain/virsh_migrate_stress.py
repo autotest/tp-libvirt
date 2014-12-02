faf26063d6268e9927d7bd3b67e3b499c6dc78bc
Yu Mingfei <yumingfei@cn.fujitsu.com>
2014-12-02 16:31:17 -0500

    libvirt.tests: Default to use unsafe for migration testcases.
    If there is not 'cache=none' for vm disks, migration will be failed:
    error: Unsafe migration: Migration may lead to data corruption if disks use cache != none
    So for those cases that do not care about safe, we can pass --unsafe by default.
    
    Signed-off-by: Yu Mingfei <yumingfei@cn.fujitsu.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_migrate_stress.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_migrate_stress.py
@@ -77,7 +77,8 @@ def do_stress_migration(vms, srcuri, desturi, stress_type,
         logging.warning("Add stress for migration failed:%s", fail_info)
 
     logging.debug("Starting migration...")
-    migrate_options = "--live --timeout %s" % params.get("virsh_migrate_timeout", 60)
+    migrate_options = ("--live --unsafe --timeout %s"
+                       % params.get("virsh_migrate_timeout", 60))
     migtest.do_migration(vms, srcuri, desturi, migration_type, options=migrate_options,
                          thread_timeout=thread_timeout)
 
