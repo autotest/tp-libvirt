7b53207ef8f4b2c5616da0f3d96f5680cd4973a5
rbian <rbian@redhat.com>
2014-12-10 11:37:51 +0800

    Add interface type "user" and "mcast" tests.

--- libvirt/tests/src/virtual_network/iface_options.py
+++ libvirt/tests/src/virtual_network/iface_options.py
@@ -4,8 +4,13 @@ import time
 import logging
 from autotest.client.shared import error
 from autotest.client import utils
-from virttest import aexpect, virt_vm, virsh, remote
+from virttest import aexpect
+from virttest import remote
+from virttest import virt_vm
+from virttest import virsh
 from virttest import utils_net
+from virttest import utils_misc
+from virttest import utils_libguestfs
 from virttest.utils_test import libvirt
 from virttest.libvirt_xml.devices.interface import Interface
 from virttest.libvirt_xml import vm_xml, xcepts
@@ -24,14 +29,16 @@ def run(test, params, env):
     """
     vm_name = params.get("main_vm")
     vm = env.get_vm(vm_name)
-    virsh_dargs = {'debug': True, 'ignore_status': True}
+    virsh_dargs = {'debug': True, 'ignore_status': False}
 
     def create_iface_xml(iface_mac):
         """
         Create interface xml file
         """
         iface = Interface(type_name=iface_type)
-        iface.source = eval(iface_source)
+        source = eval(iface_source)
+        if iface_source:
+            iface.source = source
         iface.model = iface_model if iface_model else "virtio"
         iface.mac_address = iface_mac
         driver_dict = {}
@@ -62,6 +69,13 @@ def run(test, params, env):
             iface.model = iface_model
         else:
             del iface.model
+        if iface_type:
+            iface.type_name = iface_type
+        source = eval(iface_source)
+        if iface_source:
+            iface.source = source
+        else:
+            del iface.source
         driver_dict = {}
         driver_host = {}
         driver_guest = {}
@@ -78,15 +92,37 @@ def run(test, params, env):
             del iface.address
 
         logging.debug("New interface xml file: %s", iface)
+        if unprivileged_user:
+            # Create disk image for unprivileged user
+            disk_index = xml_devices.index(
+                xml_devices.by_device_tag("disk")[0])
+            disk_xml = xml_devices[disk_index]
+            logging.debug("source: %s", disk_xml.source)
+            disk_source = disk_xml.source.attrs["file"]
+            cmd = ("cp %s %s && chmod a+rw %s" %
+                   (disk_source, dst_disk, dst_disk))
+            utils.run(cmd)
+            disk_xml.source = disk_xml.new_disk_source(
+                attrs={"file": dst_disk})
+            vmxml.devices = xml_devices
+            # Remove all channels to avoid of permission problem
+            channels = vmxml.get_devices(device_type="channel")
+            for channel in channels:
+                vmxml.del_device(channel)
+
+            vmxml.xmltreefile.write()
+            logging.debug("New VM xml: %s", vmxml)
+            utils.run("chmod a+rw %s" % vmxml.xml)
+            virsh.define(vmxml.xml, **virsh_dargs)
         # Try to modify interface xml by update-device or edit xml
-        if update:
+        elif update:
             iface.xmltreefile.write()
             # Validate the xml file
             if not iface.get_validates():
                 raise error.TestFail("Failed to validate xml file")
 
             ret = virsh.update_device(vm_name, iface.xml,
-                                      **virsh_dargs)
+                                      ignore_status=True)
             libvirt.check_exit_status(ret, status_error)
         else:
             vmxml.devices = xml_devices
@@ -94,6 +130,9 @@ def run(test, params, env):
             vmxml.sync()
 
     def check_offloads_option(if_name, driver_options, session=None):
+        """
+        Check interface offloads by ethtool output
+        """
         offloads = {"csum": "tx-checksumming",
                     "gso": "generic-segmentation-offload",
                     "tso4": "tcp-segmentation-offload",
@@ -101,8 +140,8 @@ def run(test, params, env):
                     "ecn": "tx-tcp-ecn-segmentation",
                     "ufo": "udp-fragmentation-offload"}
         if session:
-            ret, output = session.cmd_status_output("ethtool -k %s | head -18"
-                                                    % if_name)
+            ret, output = session.cmd_status_output("ethtool -k %s | head"
+                                                    " -18" % if_name)
         else:
             out = utils.run("ethtool -k %s | head -18" % if_name)
             ret, output = out.exit_status, out.stdout
@@ -200,20 +239,116 @@ def run(test, params, env):
                                      " command line" %
                                      (driver_opt, driver_dict[driver_opt]))
 
+    def check_user_network(session):
+        """
+        Check user network ip address on guest
+        """
+        vm_ips = []
+        utils_net.restart_guest_network(session)
+        wait_func = lambda: utils_net.get_guest_ip_addr(session,
+                                                        iface_mac_old)
+        # Wait for IP address is ready
+        utils_misc.wait_for(wait_func, 10)
+        vm_ips.append(utils_net.get_guest_ip_addr(session,
+                                                  iface_mac_old))
+        if attach_device:
+            # Get the additional interafce ip address
+            wait_func = lambda: utils_net.get_guest_ip_addr(session,
+                                                            iface_mac)
+            # Wait for IP address is ready
+            utils_misc.wait_for(wait_func, 10)
+            vm_ips.append(utils_net.get_guest_ip_addr(session,
+                                                      iface_mac))
+        logging.debug("IP address on guest: %s", vm_ips)
+        if len(vm_ips) != len(set(vm_ips)):
+            raise error.TestFail("Duplicated IP address on guest")
+        for vm_ip in vm_ips:
+            if vm_ip is None or not vm_ip.startswith("10.0.2."):
+                raise error.TestFail("Found wrong IP address"
+                                     "on guest")
+        # Check gateway address
+        gateway = utils_net.get_net_gateway(session.cmd_output)
+        if gateway != "10.0.2.2":
+            raise error.TestFail("The gateway on guest is not"
+                                 " right")
+        # Check dns server address
+        ns_list = utils_net.get_net_nameserver(session.cmd_output)
+        if "10.0.2.3" not in ns_list:
+            raise error.TestFail("The dns server can't be found"
+                                 " on guest")
+
+    def check_mcast_network(session):
+        """
+        Check multicast ip address on guests
+        """
+        src_addr = eval(iface_source)['address']
+        add_session = additional_vm.wait_for_login()
+        vms_sess_dict = {vm_name: session,
+                         additional_vm.name: add_session}
+
+        # Check mcast address on host
+        cmd = "netstat -g | grep %s" % src_addr
+        if utils.run(cmd, ignore_status=True).exit_status:
+            raise error.TestFail("Can't find multicast ip address"
+                                 " on host")
+        vms_ip_dict = {}
+        # Get ip address on each guest
+        for vms in vms_sess_dict.keys():
+            vm_mac = vm_xml.VMXML.get_first_mac_by_name(vms)
+            vm_ip = utils_net.get_guest_ip_addr(vms_sess_dict[vms],
+                                                vm_mac)
+            if not vm_ip:
+                raise error.TestFail("Can't get multicast ip"
+                                     " address on guest")
+            vms_ip_dict.update({vms: vm_ip})
+        if len(set(vms_ip_dict.values())) != len(vms_sess_dict):
+            raise error.TestFail("Got duplicated multicast ip address")
+        logging.debug("Found ips on guest: %s", vms_ip_dict)
+
+        # Run omping server on host
+        cmd = ("iptables -F;omping -m %s %s" %
+               (src_addr, "192.168.122.1 %s" %
+                ' '.join(vms_ip_dict.values())))
+        # Run a backgroup job waiting for connection of client
+        bgjob = utils.AsyncJob(cmd)
+
+        # Run omping client on guests
+        for vms in vms_sess_dict.keys():
+            # omping should be installed first
+            cmd = "rpm -q omping || yum install -y omping"
+            stat, output = vms_sess_dict[vms].cmd_status_output(cmd, 300)
+            logging.debug(output)
+            if stat:
+                raise error.TestFail("Failed to install omping"
+                                     " on guest")
+            cmd = ("iptables -F; omping -c 5 -T 5 -m %s %s" %
+                   (src_addr, "192.168.122.1 %s" %
+                    vms_ip_dict[vms]))
+            ret, output = vms_sess_dict[vms].cmd_status_output(cmd)
+            logging.debug("omping ret: %s, output: %s", ret, output)
+            if (not output.count('multicast, xmt/rcv/%loss = 5/5/0%') or
+                    not output.count('unicast, xmt/rcv/%loss = 5/5/0%')):
+                raise error.TestFail("omping failed on guest")
+        # Kill the backgroup job
+        bgjob.kill_func()
+
     status_error = "yes" == params.get("status_error", "no")
     start_error = "yes" == params.get("start_error", "no")
     define_error = "yes" == params.get("define_error", "no")
+    unprivileged_user = params.get("unprivileged_user")
 
     # Interface specific attributes.
     iface_type = params.get("iface_type", "network")
-    iface_source = params.get("iface_source", "{'network': 'default'}")
+    iface_source = params.get("iface_source", "{}")
     iface_driver = params.get("iface_driver")
     iface_model = params.get("iface_model")
     iface_driver_host = params.get("iface_driver_host")
     iface_driver_guest = params.get("iface_driver_guest")
+    attach_device = params.get("attach_iface_device")
     change_option = "yes" == params.get("change_iface_options", "no")
-    attach_device = "yes" == params.get("attach_iface_device", "no")
     update_device = "yes" == params.get("update_iface_device", "no")
+    additional_guest = "yes" == params.get("additional_guest", "no")
+    serail_login = "yes" == params.get("serail_login", "no")
     test_option_cmd = "yes" == params.get(
                       "test_iface_option_cmd", "no")
     test_option_xml = "yes" == params.get(
@@ -222,19 +357,45 @@ def run(test, params, env):
                      "test_vhost_net", "no")
     test_option_offloads = "yes" == params.get(
                            "test_option_offloads", "no")
+    test_iface_user = "yes" == params.get(
+                      "test_iface_user", "no")
+    test_iface_mcast = "yes" == params.get(
+                       "test_iface_mcast", "no")
 
     if iface_driver_host or iface_driver_guest:
         if not libvirt_version.version_compare(1, 2, 9):
             raise error.TestNAError("Offloading options not supported "
                                     " in this libvirt version")
 
-    # Destroy VM first.
-    if vm.is_alive():
-        vm.destroy(gracefully=False)
+    if unprivileged_user:
+        virsh_dargs["unprivileged_user"] = unprivileged_user
+        # Create unprivileged user if needed
+        cmd = ("grep {0} /etc/passwd || "
+               "useradd {0}".format(unprivileged_user))
+        utils.run(cmd)
+        # Need another disk image for unprivileged user to access
+        dst_disk = "/tmp/%s.img" % unprivileged_user
+
+    if serail_login:
+        # Set serial console for serial login
+        if vm.is_dead():
+            vm.start()
+        session = vm.wait_for_login()
+        # Set console option
+        vm.set_kernel_console("ttyS0", "115200")
+        # Shutdown here for sync fs
+        vm.shutdown()
+    else:
+        if vm.is_alive():
+            vm.destroy(gracefully=False)
 
     # Back up xml file.
     vmxml_backup = vm_xml.VMXML.new_from_inactive_dumpxml(vm_name)
-    iface_mac = vm_xml.VMXML.get_first_mac_by_name(vm_name)
+    iface_mac_old = vm_xml.VMXML.get_first_mac_by_name(vm_name)
+    # iface_mac will update if attach a new interface
+    iface_mac = iface_mac_old
+    # Additional vm for test
+    additional_vm = None
 
     # Build the xml and run test.
     try:
@@ -250,9 +411,45 @@ def run(test, params, env):
             if not utils.system(cmd, ignore_status=True):
                 raise error.TestError("Can't remove vhost_net driver")
 
+        # Attach a interface when vm is shutoff
+        if attach_device == 'config':
+            iface_mac = utils_net.generate_mac_address_simple()
+            iface_xml_obj = create_iface_xml(iface_mac)
+            iface_xml_obj.xmltreefile.write()
+            ret = virsh.attach_device(vm_name, iface_xml_obj.xml,
+                                      flagstr="--config",
+                                      ignore_status=True)
+            libvirt.check_exit_status(ret)
+
+        # Clone additional vm
+        if additional_guest:
+            guest_name = "%s_%s" % (vm_name, '1')
+            # Clone additional guest
+            utils_libguestfs.virt_clone_cmd(vm_name, guest_name, True)
+            additional_vm = vm.clone(guest_name)
+            additional_vm.start()
+            #additional_vm.wait_for_login()
+
         # Start the VM.
-        vm.start()
-        session = vm.wait_for_login()
+        if unprivileged_user:
+            virsh.start(vm_name, **virsh_dargs)
+            cmd = ("su - %s -c 'virsh console %s'"
+                   % (unprivileged_user, vm_name))
+            session = aexpect.ShellSession(cmd)
+            session.sendline()
+            remote.handle_prompts(session, params.get("username"),
+                                  params.get("password"), "[\#\$]", 30)
+            utils_net.restart_guest_network(session)
+            # Get ip address on guest
+            if not utils_net.get_guest_ip_addr(session, iface_mac):
+                raise error.TestError("Can't get ip address on guest")
+        else:
+            # Will raise VMStartError exception if start fails
+            vm.start()
+            if test_iface_user:
+                session = vm.wait_for_serial_login()
+            else:
+                session = vm.wait_for_login()
         if start_error:
             raise error.TestFail("VM started unexpectedly")
 
@@ -262,12 +459,13 @@ def run(test, params, env):
                                      " loaded automatically")
 
         # Attach a interface when vm is running
-        if attach_device:
+        if attach_device == 'live':
             iface_mac = utils_net.generate_mac_address_simple()
             iface_xml_obj = create_iface_xml(iface_mac)
             iface_xml_obj.xmltreefile.write()
             ret = virsh.attach_device(vm_name, iface_xml_obj.xml,
-                                      flagstr="", **virsh_dargs)
+                                      flagstr="--live",
+                                      ignore_status=True)
             libvirt.check_exit_status(ret)
             # Need sleep here for attachment take effect
             time.sleep(5)
@@ -293,12 +491,20 @@ def run(test, params, env):
                 check_offloads_option(ifname_guest, eval(iface_driver_guest),
                                       session)
 
-        # Detach hot-plugged interface at last
+        if test_iface_user:
+            # Test user type network
+            check_user_network(session)
+        if test_iface_mcast:
+            # Test mcast type network
+            check_mcast_network(session)
+
+        # Detach hot/cold-plugged interface at last
         if attach_device:
             ret = virsh.detach_device(vm_name, iface_xml_obj.xml,
-                                      flagstr="", **virsh_dargs)
+                                      flagstr="", ignore_status=True)
             libvirt.check_exit_status(ret)
 
+        session.close()
     except virt_vm.VMStartError, e:
         logging.error(str(e))
         if start_error:
@@ -314,7 +520,16 @@ def run(test, params, env):
 
     finally:
         # Recover VM.
+        logging.info("Restoring vm...")
+        if unprivileged_user:
+            virsh.remove_domain(vm_name, "--remove-all-storage",
+                                **virsh_dargs)
+        if additional_vm:
+            virsh.remove_domain(additional_vm.name,
+                                "--remove-all-storage")
+            # Kill all omping server process on host
+            utils.system("pidof omping && killall omping",
+                         ignore_status=True)
         if vm.is_alive():
             vm.destroy(gracefully=False)
-        logging.info("Restoring vm...")
         vmxml_backup.sync()
