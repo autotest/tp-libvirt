b7d1063a78a59a55a22bd7c5371fb97f7836ca14
Wayne Sun <gsun@redhat.com>
2014-12-17 19:29:08 +0800

    blockpull: Add various disk types for testing
    Add network disk with gluster and iscsi type, block disk use iscsi
    backend, netfs disk mounted on local for testing.
    
    Also added test case for option --async and --keep-relative.
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_blockpull.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_blockpull.py
@@ -3,7 +3,39 @@ import logging
 import tempfile
 from autotest.client.shared import error
 from virttest import virsh, data_dir
+from virttest.utils_test import libvirt
 from virttest.libvirt_xml import vm_xml
+from provider import libvirt_version
+
+
+def check_chain_xml(disk_xml, chain_lst):
+    """
+    param disk_xml: disk xmltreefile
+    param chain_lst: list, expected backing chain list
+    return: True or False
+    """
+    logging.debug("expected backing chain list is %s", chain_lst)
+    src_file = disk_xml.find('source').get('file')
+    if src_file != chain_lst[0]:
+        logging.error("Current top img %s is not expected", src_file)
+        return False
+    for i in range(1, len(chain_lst)):
+        backing_xml = disk_xml.find('backingStore')
+        src_element = backing_xml.find('source')
+        src_file = None
+        for elem in ('file', 'name', 'dev'):
+            elem_val = src_element.get(elem)
+            if elem_val:
+                src_file = elem_val
+                break
+        if src_file != chain_lst[i]:
+            logging.error("backing store chain file %s is "
+                          "not expected" % src_file)
+            return False
+        disk_xml = disk_xml.reroot('backingStore')
+        logging.debug("after reroot the xml is %s", disk_xml)
+
+    return True
 
 
 def run(test, params, env):
@@ -21,7 +53,7 @@ def run(test, params, env):
         disks = vm.get_disk_devices()
 
         # Make three external snapshots for disks only
-        for count in range(1, 4):
+        for count in range(1, 5):
             options = "snapshot%s snap%s-desc " \
                       "--disk-only --atomic --no-metadata" % (count, count)
 
@@ -57,12 +89,22 @@ def run(test, params, env):
     # Process cartesian parameters
     vm_name = params.get("main_vm")
     vm = env.get_vm(vm_name)
-    session = vm.wait_for_login()
+    needs_agent = "yes" == params.get("needs_agent", "yes")
+    replace_vm_disk = "yes" == params.get("replace_vm_disk", "no")
 
     with_timeout = ("yes" == params.get("with_timeout_option", "no"))
     status_error = ("yes" == params.get("status_error", "no"))
-    base_option = params.get("base_option", "none")
+    base_option = params.get("base_option", None)
+    keep_relative = "yes" == params.get("keep_relative", 'no')
     virsh_dargs = {'debug': True}
+
+    # Process domain disk device parameters
+    disk_type = params.get("disk_type")
+    disk_src_protocol = params.get("disk_source_protocol")
+    vol_name = params.get("vol_name")
+    tmp_dir = data_dir.get_tmp_dir()
+    pool_name = params.get("pool_name", "gluster-pool")
+    brick_path = os.path.join(tmp_dir, pool_name)
 
     # A backup of original vm
     vmxml_backup = vm_xml.VMXML.new_from_inactive_dumpxml(vm_name)
@@ -73,17 +115,35 @@ def run(test, params, env):
     if len(exsiting_snaps) != 0:
         raise error.TestFail("There are snapshots created for %s already" % vm_name)
 
+    snapshot_external_disks = []
     try:
-        # Get a tmp_dir.
-        tmp_dir = data_dir.get_tmp_dir()
+        if disk_src_protocol == 'iscsi' and disk_type == 'network':
+            if not libvirt_version.version_compare(1, 0, 4):
+                raise error.TestNAError("'iscsi' disk doesn't support in"
+                                        + " current libvirt version.")
+
+        # Set vm xml and guest agent
+        if replace_vm_disk:
+            libvirt.set_vm_disk(vm, params, tmp_dir)
+
+        if needs_agent:
+            libvirt.set_guest_agent(vm)
 
         # The first disk is supposed to include OS
         # We will perform blockpull operation for it.
         first_disk = vm.get_first_disk_devices()
-
-        snapshot_external_disks = []
+        blk_source = first_disk['source']
+        blk_target = first_disk['target']
         snapshot_flag_files = []
+
+        # get a vm session before snapshot
+        session = vm.wait_for_login()
+        # do snapshot
         make_disk_snapshot()
+
+        # snapshot src file list
+        snap_src_lst = [blk_source]
+        snap_src_lst += snapshot_external_disks
 
         blockpull_options = "--wait --verbose"
 
@@ -91,28 +151,66 @@ def run(test, params, env):
             blockpull_options += " --timeout 1"
 
         base_image = None
-        basename = os.path.basename(first_disk['source'])
+        basename = os.path.basename(blk_source)
         diskname = basename.split(".")[0]
         if base_option == "shallow":
-            base_image = os.path.join(tmp_dir, "%s.snap2" % diskname)
-        elif base_option == "base":
-            base_image = os.path.join(tmp_dir, "%s.snap1" % diskname)
-        elif base_option == "top":
             base_image = os.path.join(tmp_dir, "%s.snap3" % diskname)
+        elif base_option == "base":
+            base_image = os.path.join(tmp_dir, "%s.snap2" % diskname)
+        elif base_option == "top":
+            base_image = os.path.join(tmp_dir, "%s.snap4" % diskname)
+        elif base_option == "async":
+            blockpull_options += " --async"
 
-        if base_option != "none":
+        if base_option and base_image:
             blockpull_options += " --base %s" % base_image
 
+        if keep_relative:
+            blockpull_options += " --keep-relative"
+
         # Run test case
-        result = virsh.blockpull(vm_name, first_disk['target'],
+        result = virsh.blockpull(vm_name, blk_target,
                                  blockpull_options, **virsh_dargs)
         status = result.exit_status
 
         # Check status_error
-        if status_error and status == 0:
-            raise error.TestFail("Expect fail, but run successfully!")
-        elif not status_error and status != 0:
-            raise error.TestFail("Run failed with right command")
+        libvirt.check_exit_status(result, status_error)
+
+        if not status and not with_timeout:
+            vmxml = vm_xml.VMXML.new_from_dumpxml(vm_name)
+            disks = vmxml.devices.by_device_tag('disk')
+            for disk in disks:
+                if disk.target['dev'] != blk_target:
+                    continue
+                else:
+                    disk_xml = disk.xmltreefile
+                    break
+
+            logging.debug("after pull the disk xml is: %s"
+                          % disk_xml)
+            if libvirt_version.version_compare(1, 2, 4):
+                err_msg = "Domain image backing chain check failed"
+                if not base_option:
+                    chain_lst = snap_src_lst[-1:]
+                    ret = check_chain_xml(disk_xml, chain_lst)
+                    if not ret:
+                        raise error.TestFail(err_msg)
+                elif "shallow" in base_option:
+                    chain_lst = snap_src_lst[::-1]
+                    ret = check_chain_xml(disk_xml, chain_lst)
+                    if not ret:
+                        raise error.TestFail(err_msg)
+                elif "base" in base_option:
+                    chain_lst = snap_src_lst[::-1]
+                    base_index = chain_lst.index(base_image)
+                    val_tmp = []
+                    for i in range(1, base_index):
+                        val_tmp.append(chain_lst[i])
+                    for i in val_tmp:
+                        chain_lst.remove(i)
+                    ret = check_chain_xml(disk_xml, chain_lst)
+                    if not ret:
+                        raise error.TestFail(err_msg)
 
         # If base image is the top layer of snapshot chain,
         # virsh blockpull should fail, return directly
@@ -126,9 +224,20 @@ def run(test, params, env):
                 raise error.TestFail("blockpull failed: %s" % output)
 
     finally:
+        if vm.is_alive():
+            vm.destroy(gracefully=False)
+        # Recover xml of vm.
+        vmxml_backup.sync("--snapshots-metadata")
+
         for disk in snapshot_external_disks:
             if os.path.exists(disk):
                 os.remove(disk)
 
-        # Recover xml of vm.
-        vmxml_backup.sync()
+        if disk_src_protocol == 'iscsi':
+            libvirt.setup_or_cleanup_iscsi(is_setup=False)
+        elif disk_src_protocol == 'gluster':
+            libvirt.setup_or_cleanup_gluster(False, vol_name, brick_path)
+        elif disk_src_protocol == 'netfs':
+            restore_selinux = params.get('selinux_status_bak')
+            libvirt.setup_or_cleanup_nfs(is_setup=False,
+                                         restore_selinux=restore_selinux)
