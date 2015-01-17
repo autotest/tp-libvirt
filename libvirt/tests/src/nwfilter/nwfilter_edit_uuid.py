7d42fe90877c52f3714e1d2162617ed83eb9f586
Wayne Sun <gsun@redhat.com>
2015-01-17 16:59:58 +0800

    nwfilter_edit_uuid: Add edit uuid case
    Since commit 46a811d, update uuid should fail, check more info in:
    https://bugzilla.redhat.com/show_bug.cgi?id=1077009
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

---
+++ libvirt/tests/src/nwfilter/nwfilter_edit_uuid.py
@@ -0,0 +1,64 @@
+import logging
+from autotest.client.shared import error
+from virttest import virsh
+from virttest import libvirt_xml
+from virttest import aexpect
+from virttest import remote
+from provider import libvirt_version
+
+
+def run(test, params, env):
+    """
+    Test virsh nwfilter-edit with uuid.
+
+    1) Prepare parameters.
+    2) Run nwfilter-edit command.
+    3) Check result.
+    4) Clean env
+    """
+    # Prepare parameters
+    filter_name = params.get("edit_filter_name", "")
+    status_error = params.get("status_error", "no")
+    new_uuid = "11111111-1111-1111-1111-111111111111"
+    edit_cmd = ":2s/<uuid>.*$/<uuid>%s<\/uuid>/" % new_uuid
+
+    # Since commit 46a811d, the logic changed for not allow update filter
+    # uuid, so decide status_error with libvirt version.
+    if libvirt_version.version_compare(1, 2, 7):
+        status_error = True
+    else:
+        status_error = False
+
+    # Backup filter xml
+    new_filter = libvirt_xml.NwfilterXML()
+    filterxml = new_filter.new_from_filter_dumpxml(filter_name)
+    logging.debug("the filter xml is: %s" % filterxml.xmltreefile)
+
+    try:
+        # Run command
+        session = aexpect.ShellSession("sudo -s")
+        try:
+            session.sendline("virsh nwfilter-edit %s" % filter_name)
+            session.sendline(edit_cmd)
+            # Press ESC
+            session.send('\x1b')
+            # Save and quit
+            session.send('ZZ')
+            remote.handle_prompts(session, None, None, r"[\#\$]\s*$")
+            session.close()
+            if not status_error:
+                logging.info("Succeed to do nwfilter edit")
+            else:
+                raise error.TestFail("edit uuid should fail but got succeed.")
+        except (aexpect.ShellError, aexpect.ExpectError, remote.LoginTimeoutError), details:
+            log = session.get_output()
+            session.close()
+            if "Try again? [y,n,f,?]:" in log and status_error:
+                logging.debug("edit uuid failed as expected.")
+            else:
+                raise error.TestFail("Failed to do nwfilter-edit: %s\n%s"
+                                     % (details, log))
+    finally:
+        # Clean env
+        virsh.nwfilter_undefine(filter_name, debug=True)
+        virsh.nwfilter_define(filterxml.xml, debug=True)
