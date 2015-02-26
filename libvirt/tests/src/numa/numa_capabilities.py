23c6d082967448f06c6d493c989496133932cd41
Wayne Sun <gsun@redhat.com>
2015-02-26 16:54:53 +0800

    numa_capabilities: Check numa info in capabilites
    Check node distance and cell topology info in capabilites xml.
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

---
+++ libvirt/tests/src/numa/numa_capabilities.py
@@ -0,0 +1,46 @@
+import logging
+from virttest import libvirt_xml
+from virttest import utils_libvirtd
+from virttest import utils_misc
+from autotest.client.shared import error
+
+
+def run(test, params, env):
+    """
+    Test capabilities with host numa node topology
+    """
+    libvirtd = utils_libvirtd.Libvirtd()
+    libvirtd.start()
+    try:
+        new_cap = libvirt_xml.CapabilityXML()
+        if not libvirtd.is_running():
+            raise error.TestFail("Libvirtd is not running")
+        topo = new_cap.cells_topology
+        logging.debug("topo xml is %s", topo.xmltreefile)
+        cell_list = topo.get_cell()
+        numa_info = utils_misc.NumaInfo()
+        for cell_num in range(len(cell_list)):
+            # check node distances
+            cell_distance = cell_list[cell_num].sibling
+            logging.debug("cell %s distance is %s", cell_num, cell_distance)
+            node_distance = numa_info.distances[cell_num]
+            for j in range(len(cell_list)):
+                if cell_distance[j]['value'] != node_distance[j]:
+                    raise error.TestFail("cell distance value not expected.")
+            # check node cell cpu
+            cell_xml = cell_list[cell_num]
+            cpu_list_from_xml = cell_xml.cpu
+            node_ = numa_info.nodes[cell_num]
+            cpu_list = node_.cpus
+            logging.debug("cell %s cpu list is %s", cell_num, cpu_list)
+            cpu_topo_list = []
+            for cpu_id in cpu_list:
+                cpu_dict = node_.get_cpu_topology(cpu_id)
+                cpu_topo_list.append(cpu_dict)
+            logging.debug("cpu topology list from capabilities xml is %s",
+                          cpu_list_from_xml)
+            if cpu_list_from_xml != cpu_topo_list:
+                raise error.TestFail("cpu list %s from capabilities xml not "
+                                     "expected.")
+    finally:
+        libvirtd.restart()
