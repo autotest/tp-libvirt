98de10ce8855e1a3c8380c77656e1a7250804d81
sudeesh john <sudeesh@linux.vnet.ibm.com>
2017-01-06 11:27:47 +0530

    In Ubuntu SELinux is not work at the moment. So Skipping testcases which need the SELinux to be enabled and in Enforcing mode. This patch will skip(insted of ERRORing out) the the testcase if SELinux is disabled mode.
    Signed-off-by: Sudeesh John sudeesh@linux.vnet.ibm.com

--- libvirt/tests/src/svirt/dac_start_destroy.py
+++ libvirt/tests/src/svirt/dac_start_destroy.py
@@ -147,6 +147,8 @@ def run(test, params, env):
 
     # Set selinux of host.
     backup_sestatus = utils_selinux.get_status()
+    if backup_sestatus == "disabled":
+        raise error.TestNAError("SKIP: SELinux is set to enforcing")
     utils_selinux.set_status(host_sestatus)
 
     def _create_user():
