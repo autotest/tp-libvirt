b811f9123358118ceed0cd802d14156873abffc3
Yanbing Du <ydu@redhat.com>
2017-04-11 14:51:29 +0800

    pool_edit: bug fix and remove invalid cases
    Use existing pool for testing may cause lots of problems, so let's just
    prepare a new pool.
    After edit disk pool source format, we have to rebuild it to overwrite
    the disk partition table.
    By default, disk pool source format is 'dos', and edit it to do 'dos'
    make no sense and cause case fail, so remove it.
    And simple edit source path to redefine the pool it not applicable
    to gluster pool, so also remove it.
    
    Signed-off-by: Yanbing Du <ydu@redhat.com>

--- libvirt/tests/src/virsh_cmd/pool/virsh_pool_edit.py
+++ libvirt/tests/src/virsh_cmd/pool/virsh_pool_edit.py
@@ -12,8 +12,6 @@ from virttest import remote
 from virttest.libvirt_xml import pool_xml
 from virttest.utils_test import libvirt
 
-from provider import libvirt_version
-
 
 def edit_pool(pool, edit_cmd):
     """
@@ -25,7 +23,6 @@ def edit_pool(pool, edit_cmd):
     session = aexpect.ShellSession("sudo -s")
     try:
         session.sendline("virsh pool-edit %s" % pool)
-        logging.info("edit_cmd: %s", edit_cmd)
         for cmd in edit_cmd:
             logging.info("cmd: %s", cmd)
             session.sendline(cmd)
@@ -87,22 +84,13 @@ def run(test, params, env):
     pool_ref = params.get("pool_ref", "name")
     pool_name = params.get("pool_name", "default")
     pool_uuid = params.get("pool_uuid", "")
-    pool_exist = "yes" == params.get("pool_exist", "yes")
     status_error = "yes" == params.get("status_error", "no")
     pool_type = params.get("pool_type", "dir")
     pool_target = os.path.join(data_dir.get_tmp_dir(),
                                params.get("pool_target", "pool_target"))
-    source_name = params.get("pool_source_name", "gluster-vol1")
-    source_path = params.get("pool_source_path", "/")
     emulated_image = params.get("emulated_image", "emulated-image-disk")
     edit_target = params.get("edit_target", "target_path")
 
-    if not libvirt_version.version_compare(1, 0, 0):
-        if pool_type == "gluster":
-            raise error.TestNAError("Gluster pool is not supported in current"
-                                    " libvirt version.")
-
-    redefine_pool_flag = False
     pool = pool_name
     if pool_ref == "uuid":
         pool = pool_uuid
@@ -112,20 +100,10 @@ def run(test, params, env):
     check_pool_name = pool_name
     new_path = ""
     try:
-        if pool_exist and not status_error:
-            if libvirt_pool.pool_exists(pool_name):
-                logging.debug("Find pool '%s' to edit.", pool_name)
-                redefine_pool_flag = True
-            else:
-                logging.debug("Define pool '%s' as it not exist", pool_name)
-                if pool_type == "gluster":
-                    poolvolune_test.pre_pool(pool_name, pool_type, pool_target,
-                                             emulated_image,
-                                             source_name=source_name,
-                                             source_path=source_path)
-                else:
-                    poolvolune_test.pre_pool(pool_name, pool_type, pool_target,
-                                             emulated_image)
+        if not status_error:
+            # Always prepare a pool for testing
+            poolvolune_test.pre_pool(pool_name, pool_type, pool_target,
+                                     emulated_image)
             if not pool_uuid and pool_ref == "uuid":
                 pool = libvirt_pool.get_pool_uuid(pool_name)
             poolxml.xml = pool_xml.PoolXML().new_from_dumpxml(pool_name).xml
@@ -145,7 +123,7 @@ def run(test, params, env):
             # Test: Edit disk pool format type:
             elif edit_target == "pool_format_type":
                 edit_cmd = []
-                new_format_type = params.get("pool_format", "dos")
+                new_format_type = params.get("pool_format", "gpt")
                 edit_cmd.append(":%s/<format type=.*\/>/<format type='" +
                                 new_format_type + "'\/>/")
                 expect_value = new_format_type
@@ -167,37 +145,25 @@ def run(test, params, env):
                 raise error.TestNAError("No edit method for %s" % edit_target)
 
             # run test and check the result
-            logging.info("pool=%s", pool)
             edit_pool(pool, edit_cmd)
             if libvirt_pool.is_pool_active(pool_name):
                 libvirt_pool.destroy_pool(pool_name)
+            # After change the source format, we have to rebuild the pool to
+            # overwrite the disk partition table
+            if edit_target == "pool_format_type":
+                virsh.pool_build(pool_name, '--overwrite', debug=True)
             if not libvirt_pool.start_pool(check_pool_name):
-                raise error.TestFail("Fail to start pool after edit it.")
+                raise error.TestFail("Fail to start pool after edit it")
             if not check_pool(check_pool_name, edit_target, expect_value):
-                raise error.TestFail("Edit pool fail")
-        elif not pool_exist and not status_error:
-            raise error.TestFail("Conflict condition: pool not exist and expect "
-                                 "pool edit succeed.")
+                raise error.TestFail("Edit pool check fail")
         else:
             # negative test
-            output = virsh.pool_edit(pool)
-            if output.exit_status:
-                logging.info("Fail to do pool edit as expect: %s",
-                             output.stderr.strip())
-            else:
-                redefine_pool_flag = True
-                raise error.TestFail("Expect fail but do pool edit succeed.")
+            result = virsh.pool_edit(pool)
+            libvirt.check_exit_status(result, status_error)
     finally:
         for pool in [pool_name, check_pool_name]:
             if libvirt_pool.pool_exists(pool):
                 poolvolune_test.cleanup_pool(check_pool_name, pool_type,
-                                             pool_target, emulated_image,
-                                             source_name=source_name)
-        if redefine_pool_flag:
-            try:
-                # poolxml could be empty if error happened when define pool
-                poolxml.pool_define()
-            finally:
-                logging.error("Recover pool %s failed", pool_name)
+                                             pool_target, emulated_image)
         if os.path.exists(new_path):
             os.rmdir(new_path)
