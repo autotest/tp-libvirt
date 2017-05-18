3c0ca3468214681f2a1d3129b61f760abcc7876f
Dan Zheng <dzheng@redhat.com>
2017-05-18 11:23:33 +0800

    virsh.migrate: Fix the postcopy option
    Incorrect logic causes '--postcopy' is not inserted into the migration
    options for '--postcopy_after_precopy'
    
    Signed-off-by: Dan Zheng <dzheng@redhat.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_migrate.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_migrate.py
@@ -508,7 +508,7 @@ def run(test, params, env):
 
     # For --postcopy enable
     postcopy_options = params.get("postcopy_options")
-    if postcopy_options and not options.count(postcopy_options):
+    if postcopy_options:
         options = "%s %s" % (options, postcopy_options)
 
     src_uri = params.get("virsh_migrate_connect_uri")
