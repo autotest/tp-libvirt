1f41e0df7f8aec08ef5ac8cf43a279d71cc4c6fb
Jingjing Shao <jishao@redhat.com>
2017-08-23 21:24:57 +0800

    Dump/reset the specific nodedevice from different host on testing not from cfg
    (1)For different hosts have different devices, device address is not the
    same one written in cfg, this patch can get the device_name from the host on testing not from the cfg.
    
    (2)Comment the action of reset active_device, which will be tested in
    SR-IOV function, so comment the case at first.
    
    (3) Delete the active device case from nodedev_reset , this part will be
    in SRIOV test case.
    
    Signed-off-by: Jingjing Shao <jishao@redhat.com>

--- libvirt/tests/src/virsh_cmd/nodedev/virsh_nodedev_dumpxml.py
+++ libvirt/tests/src/virsh_cmd/nodedev/virsh_nodedev_dumpxml.py
@@ -1,80 +1,91 @@
 import logging
-
+import time
+import os
+import commands
 from autotest.client import utils
-from autotest.client.shared import error
-
 from virttest import virsh
 from virttest.libvirt_xml import nodedev_xml
-
 from provider import libvirt_version
-
-
-def do_nodedev_dumpxml(dev_name, dev_opt="", **dargs):
-    """
-    Do dumpxml and check the result.
-
-    (1).execute nodedev-dumpxml command.
-    (2).compare info in xml with info in sysfs.
-
-    :param dev_name: name of device.
-    :param dev_opt: command extra options
-    :param dargs: extra dict args
-    :raise TestFail: if execute command failed
-                     or check result failed.
-    """
-    result = virsh.nodedev_dumpxml(dev_name, options=dev_opt, **dargs)
-    if result.exit_status:
-        raise error.TestError("Dumpxml node device %s failed.\n"
-                              "Detail:%s." % (dev_name, result.stderr))
-    logging.debug('Executing "virsh nodedev-dumpxml %s" finished.', dev_name)
-    # compare info in xml with info in sysfs.
-    nodedevxml = nodedev_xml.NodedevXML.new_from_dumpxml(dev_name)
-    if not nodedevxml.validates:
-        raise error.TestError("nodedvxml of %s is not validated." % (dev_name))
-    # Get the dict of key to value in xml.
-    # key2value_dict_xml contain the all keys and values in xml need checking.
-    key2value_dict_xml = nodedevxml.get_key2value_dict()
-    # Get the dict of key to path in sysfs.
-    # key2syspath_dict contain the all keys and the path of file which contain
-    #                 information for each key.
-    key2syspath_dict = nodedevxml.get_key2syspath_dict()
-    # Get the values contained in files.
-    # key2value_dict_sys contain the all keys and values in sysfs.
-    key2value_dict_sys = {}
-    for key, filepath in key2syspath_dict.items():
-        value = utils.read_one_line(filepath)
-        key2value_dict_sys[key] = value
-
-    # Compare the value in xml and in syspath.
-    for key in key2value_dict_xml:
-        value_xml = key2value_dict_xml.get(key)
-        value_sys = key2value_dict_sys.get(key)
-        if not value_xml == value_sys:
-            if (key == 'numa_node' and not
-                    libvirt_version.version_compare(1, 2, 5)):
-                logging.warning("key: %s in xml is not supported yet" % key)
-            else:
-                raise error.TestError("key: %s in xml is %s,"
-                                      "but in sysfs is %s." %
-                                      (key, value_xml, value_sys))
-        else:
-            continue
-
-    logging.debug("Compare info in xml and info in sysfs finished"
-                  "for device %s.", dev_name)
+from virttest.utils_test import libvirt
 
 
 def run(test, params, env):
     """
     Test command virsh nodedev-dumpxml.
-
-    (1).get param from params.
-    (2).do nodedev dumpxml.
-    (3).clean up.
+    step1.get param from params.
+    step2.do nodedev dumpxml.
+    step3.clean up.
     """
+
+    def dump_nodedev_xml(dev_name, dev_opt="", **dargs):
+        """
+        Do dumpxml and check the result.
+
+        step1.execute nodedev-dumpxml command.
+        step1.compare info in xml with info in sysfs.
+
+        :param dev_name: name of device.
+        :param dev_opt: command extra options
+        :param dargs: extra dict args
+        """
+        result = virsh.nodedev_dumpxml(dev_name, options=dev_opt, **dargs)
+        libvirt.check_exit_status(result)
+        logging.debug('Executing "virsh nodedev-dumpxml %s" finished.', dev_name)
+        # Compare info in xml with info in sysfs.
+        nodedevice_xml = nodedev_xml.NodedevXML.new_from_dumpxml(dev_name)
+
+        if not nodedevice_xml.validates:
+            test.error("nodedvxml of %s is not validated." % (dev_name))
+        # Get the dict of key to value in xml.
+        # nodedev_dict_xml contain the all keys and values in xml need checking.
+        nodedev_dict_xml = nodedevice_xml.get_key2value_dict()
+
+        # Get the dict of key to path in sysfs.
+        # nodedev_syspath_dict contain the all keys and the path of file which contain
+        #                 information for each key.
+        nodedev_syspath_dict = nodedevice_xml.get_key2syspath_dict()
+
+        # Get the values contained in files.
+        # nodedev_dict_sys contain the all keys and values in sysfs.
+        nodedev_dict_sys = {}
+        for key, filepath in nodedev_syspath_dict.items():
+            value = utils.read_one_line(filepath)
+            nodedev_dict_sys[key] = value
+
+        # Compare the value in xml and in syspath.
+        for key in nodedev_dict_xml:
+            xml_value = nodedev_dict_xml.get(key)
+            sys_value = nodedev_dict_sys.get(key)
+
+            if not xml_value == sys_value:
+                if (key == 'numa_node' and not
+                        libvirt_version.version_compare(1, 2, 5)):
+                    logging.warning("key: %s in xml is not supported yet" % key)
+                else:
+                    test.error("key: %s in xml is %s,"
+                               "but in sysfs is %s." %
+                               (key, xml_value, sys_value))
+            else:
+                continue
+
+        logging.debug("Compare info in xml and info in sysfs finished"
+                      "for device %s.", dev_name)
+
+    def pci_devices_address():
+        """
+        Get the address of pci device
+        """
+        pci_list = virsh.nodedev_list(tree='', cap='pci')
+        pci_devices_address = pci_list.stdout.strip().splitlines()
+        pci_device_address = pci_devices_address[0]
+        return pci_device_address
+
     # Init variables.
     status_error = ('yes' == params.get('status_error', 'no'))
-    device_name = params.get('nodedev_device_name', None)
+    device_name = params.get('nodedev_device_name', 'ENTER.YOUR.PCI.DEVICE')
+    if device_name.find('ENTER.YOUR.PCI.DEVICE') != -1:
+        replace_name = pci_devices_address().strip()
+        device_name = device_name.replace('ENTER.YOUR.PCI.DEVICE', replace_name).strip()
     device_opt = params.get('nodedev_device_opt', "")
 
     # acl polkit params
@@ -86,25 +97,30 @@ def run(test, params, env):
 
     if not libvirt_version.version_compare(1, 1, 1):
         if params.get('setup_libvirt_polkit') == 'yes':
-            raise error.TestNAError("API acl test not supported in current"
-                                    " libvirt version.")
+            test.cancel("API acl test not supported in current"
+                        " libvirt version.")
 
     virsh_dargs = {}
     if params.get('setup_libvirt_polkit') == 'yes':
         virsh_dargs['unprivileged_user'] = unprivileged_user
         virsh_dargs['uri'] = uri
 
+    # change the polkit rule
+    polkit_file = "/etc/polkit-1/rules.d/500-libvirt-acl-virttest.rules"
+    if os.path.exists(polkit_file):
+        replace_cmd = "sed -i 's/'ENTER.YOUR.PCI.DEVICE'/%s/g' /etc/polkit-1/rules.d/500-libvirt-acl-virttest.rules" % device_name
+        cat_cmd = "cat /etc/polkit-1/rules.d/500-libvirt-acl-virttest.rules"
+        replace_output = commands.getoutput(replace_cmd)
+        cat_output = commands.getoutput(cat_cmd)
+
     # do nodedev dumpxml.
     try:
-        do_nodedev_dumpxml(dev_name=device_name, dev_opt=device_opt,
-                           **virsh_dargs)
+        time.sleep(10)
+        dump_nodedev_xml(dev_name=device_name, dev_opt=device_opt,
+                         **virsh_dargs)
         if status_error:
-            raise error.TestFail('Nodedev dumpxml successed in negative test.')
-        else:
-            pass
-    except error.TestError, e:
-        if status_error:
-            pass
-        else:
-            raise error.TestFail('Nodedev dumpxml failed in positive test.'
-                                 'Error: %s' % e)
+            test.fail('Nodedev dumpxml successed in negative test.')
+    except Exception, e:
+        if not status_error:
+            test.fail('Nodedev dumpxml failed in positive test.'
+                      'Error: %s' % e)
