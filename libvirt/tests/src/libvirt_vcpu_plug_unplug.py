913d4291daaf50b0fcd5bbfc044a3ee4c2dc3df6
Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
2018-05-17 14:23:43 +0530

    Add vcpu hotplug with VM reboot test scenario
    Add vcpu hotplug with VM reboot test scenario
    
    Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>

--- libvirt/tests/src/libvirt_vcpu_plug_unplug.py
+++ libvirt/tests/src/libvirt_vcpu_plug_unplug.py
@@ -76,6 +76,8 @@ def run(test, params, env):
             elif vm_operation == "suspend":
                 result = virsh.suspend(vm_name, ignore_status=True, debug=True)
                 libvirt.check_exit_status(result)
+            elif vm_operation == "reboot":
+                vm.reboot()
             else:
                 logging.debug("No operation for the domain")
 
@@ -99,6 +101,8 @@ def run(test, params, env):
             elif vm_operation == "suspend":
                 result = virsh.resume(vm_name, ignore_status=True, debug=True)
                 libvirt.check_exit_status(result)
+            elif vm_operation == "reboot":
+                pass
             else:
                 logging.debug("No need recover the domain")
 
