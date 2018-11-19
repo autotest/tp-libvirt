c0750f73b2dbb3e19ec0197d7221b41cb8a73aa0
Yan Li <yannli@redhat.com>
2018-11-19 16:44:04 +0800

    virsh_remote: need use server session instead of vm session
    The old script use vm session for remote test, but it's not
    applicable when testing on remote server. So change to
    remote.wait_for_login to compat all cases
    
    Signed-off-by: Yan Li <yannli@redhat.com>

--- libvirt/tests/src/virsh_cmd/host/virsh_hostname.py
+++ libvirt/tests/src/virsh_cmd/host/virsh_hostname.py
@@ -1,10 +1,10 @@
 from avocado.utils import process
 
 from virttest import virsh
-from virttest import utils_libvirtd
 from virttest import ssh_key
 from virttest import utils_package
 from virttest import utils_libvirtd
+from virttest import remote
 
 
 def run(test, params, env):
@@ -15,17 +15,23 @@ def run(test, params, env):
     (2) Call virsh hostname with an unexpected option
     (3) Call virsh hostname with libvirtd service stop
     """
-
-    hostname_result = process.run("hostname -f", shell=True, ignore_status=True)
-    hostname = hostname_result.stdout_text.strip()
-
+    remote_ip = params.get("remote_ip")
+    remote_pwd = params.get("remote_pwd", None)
+    remote_user = params.get("remote_user", "root")
     remote_uri = params.get("remote_uri", None)
-    vm_name = params.get("main_vm")
-    vm = env.get_vm(vm_name)
+
+    if remote_uri and remote_ip.count("EXAMPLE"):
+        test.cancel("Pls configure rempte_ip first")
+
     session = None
     if remote_uri:
-        session = vm.wait_for_login()
+        session = remote.wait_for_login('ssh', remote_ip, '22',
+                                        remote_user, remote_pwd,
+                                        r"[\#\$]\s*$")
         hostname = session.cmd_output("hostname -f").strip()
+    else:
+        hostname_result = process.run("hostname -f", shell=True, ignore_status=True)
+        hostname = hostname_result.stdout_text.strip()
 
     # Prepare libvirtd service on local
     check_libvirtd = "libvirtd" in params
@@ -42,15 +48,9 @@ def run(test, params, env):
         libvirtd.restart()
 
     # Run test case
-    option = params.get("virsh_hostname_options")
-    remote_ip = params.get("remote_ip")
-    if remote_uri and remote_ip.count("EXAMPLE"):
-        test.cancel("Pls configure rempte_ip first")
-    remote_pwd = params.get("remote_pwd", None)
-    remote_user = params.get("remote_user", "root")
     if remote_uri:
         ssh_key.setup_ssh_key(remote_ip, remote_user, remote_pwd)
-
+    option = params.get("virsh_hostname_options")
     hostname_test = virsh.hostname(option, uri=remote_uri,
                                    ignore_status=True,
                                    debug=True)
