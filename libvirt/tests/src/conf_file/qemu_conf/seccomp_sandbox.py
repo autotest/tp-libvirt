adc61cd7906960feb6b796e576a409385d60eca7
Yan Fu <yafu@redhat.com>
2018-12-24 17:02:24 +0800

    Fix the default setting of sandbox after libvirt-3.9.0
    Signed-off-by: Yan Fu <yafu@redhat.com>

--- libvirt/tests/src/conf_file/qemu_conf/seccomp_sandbox.py
+++ libvirt/tests/src/conf_file/qemu_conf/seccomp_sandbox.py
@@ -6,6 +6,8 @@ from avocado.utils import process
 from virttest import utils_config
 from virttest import utils_libvirtd
 
+from provider import libvirt_version
+
 
 def run(test, params, env):
     """
@@ -37,21 +39,25 @@ def run(test, params, env):
                 return match.groups()[0]
 
     vm_name = params.get("main_vm", "avocado-vt-vm1")
-    expected_result = params.get("expected_result", "off")
-    seccomp_sandbox = params.get("seccomp_sandbox", "off")
-    default_value = params.get("default_value", "false")
+    seccomp_sandbox = params.get("seccomp_sandbox", "")
+    default_value = "yes" == params.get("default_value", "no")
+    if libvirt_version.version_compare(3, 9, 0):
+        expected_result = params.get("expected_result", "on")
+        if expected_result == "on":
+            expected_result = "on,obsolete=deny,elevateprivileges=deny,spawn=deny,resourcecontrol=deny"
+    else:
+        expected_result = params.get("expected_result", "")
     vm = env.get_vm(vm_name)
 
     # Get old qemu -sandbox option.
     orig_qemu_sandbox = get_qemu_command_sandbox_option(vm)
-    orig_qemu_sandbox = orig_qemu_sandbox.split(',')[0]
     logging.debug('Original "-sandbox" option of qemu command is '
                   '"%s".' % orig_qemu_sandbox)
 
     config = utils_config.LibvirtQemuConfig()
     libvirtd = utils_libvirtd.Libvirtd()
     try:
-        if default_value == "true":
+        if not default_value:
             config.seccomp_sandbox = seccomp_sandbox
 
         # Restart libvirtd to make change valid.
