7a213bd18c49ccd6f135a7aac131036543337a78
jgao <jgao@localhost.localdomain>
2020-01-15 16:15:19 +0800

    Remove the pool autostart management file so that we can test the status of pool without rebooting the host.
    Signed-off-by: jgao <jgao@localhost.localdomain>

--- libvirt/tests/src/virsh_cmd/pool/virsh_pool_autostart.py
+++ libvirt/tests/src/virsh_cmd/pool/virsh_pool_autostart.py
@@ -190,6 +190,9 @@ def run(test, params, env):
             # Step(3)
             # Restart libvirtd and check pool status
             logging.info("Try to restart libvirtd")
+            # Remove the autostart management file
+            cmd = ("rm -rf /var/run/libvirt/storage/autostarted")
+            process.run(cmd, ignore_status=True, shell=True)
             libvirtd = utils_libvirtd.Libvirtd()
             libvirtd.restart()
             check_pool(pool_name, pool_type, checkpoint="State",
