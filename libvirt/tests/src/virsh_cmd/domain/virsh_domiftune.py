6e124070e70f0295b25ff63aff88a1a6dacf8d0d
Yanqiu Zhang <yanqzhan@redhat.com>
2021-07-29 05:04:49 -0400

    domiftune: add 4 cases
    Add 4 cases in domiftune to complete automation of RHEL7-45987.
    
    Signed-off-by: Yanqiu Zhang <yanqzhan@redhat.com>

--- libvirt/tests/src/virsh_cmd/domain/virsh_domiftune.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_domiftune.py
@@ -4,7 +4,7 @@ import re
 from virttest import virsh
 from virttest import utils_libvirtd
 from virttest import libvirt_version
-from virttest.libvirt_xml import vm_xml
+from virttest.libvirt_xml import vm_xml, network_xml
 from virttest.utils_test import libvirt
 
 
@@ -24,27 +24,33 @@ def check_domiftune(params, test_clear):
     test_outbound = "yes" == params.get("test_outbound", "no")
     test_inbound = "yes" == params.get("test_inbound", "no")
     status_error = 'yes' == params.get("status_error", "no")
+    later_start = 'yes' == params.get("later_start", "no")
     average_inbound_from_cmd_output = None
     average_outbound_from_cmd_output = None
     peak_inbound_from_cmd_output = None
     peak_outbound_from_cmd_output = None
     burst_inbound_from_cmd_output = None
     burst_outbound_from_cmd_output = None
+    floor_inbound_from_cmd_output = None
     average_inbound_from_xml = None
     average_outbound_from_xml = None
     peak_inbound_from_xml = None
     peak_outbound_from_xml = None
     burst_inbound_from_xml = None
     burst_outbound_from_xml = None
+    floor_inbound_from_xml = None
+    netfloor = params.get("netfloor")
 
     logging.debug("Checking inbound=%s outbound= %s", inbound, outbound)
     # get inbound and outbound parameters from virsh cmd setting
     if not status_error and test_inbound:
         in_list = re.findall(r'[0-9]+', inbound)
         inbound_average = int(in_list[0])
-        if len(in_list) == 3:
+        if len(in_list) >= 3:
             inbound_peak = int(in_list[1])
             inbound_burst = int(in_list[2])
+            if netfloor:
+                inbound_floor = int(in_list[3])
     if not status_error and test_outbound:
         out_list = re.findall(r'[0-9]+', outbound)
         outbound_average = int(out_list[0])
@@ -57,14 +63,18 @@ def check_domiftune(params, test_clear):
         vm.destroy()
         vm.start()
 
+    # setting for shutoff guest need start to check
+    if later_start:
+        vm.start()
+
     # get inbound and outbound parameters from virsh cmd output
     if vm and vm.is_alive():
         result = virsh.domiftune(vm_name, interface, options=options)
         dicts = {}
-        o = result.stdout.strip().split("\n")
-        for l in o:
-            if l and l.find(':'):
-                k, v = l.split(':')
+        bws = result.stdout.strip().split("\n")
+        for bw in bws:
+            if bw and bw.find(':'):
+                k, v = bw.split(':')
                 dicts[k.strip()] = v.strip()
 
         logging.debug(dicts)
@@ -74,12 +84,15 @@ def check_domiftune(params, test_clear):
         peak_outbound_from_cmd_output = int(dicts['outbound.peak'])
         burst_inbound_from_cmd_output = int(dicts['inbound.burst'])
         burst_outbound_from_cmd_output = int(dicts['outbound.burst'])
+        floor_inbound_from_cmd_output = int(dicts['inbound.floor'])
 
         logging.debug("inbound and outbound from cmd output:")
-        logging.debug("inbound: %s,%s,%s; outbound: %s,%s,%s"
-                      % (average_inbound_from_cmd_output, peak_inbound_from_cmd_output,
-                         burst_inbound_from_cmd_output, average_outbound_from_cmd_output,
-                         peak_outbound_from_cmd_output, burst_outbound_from_cmd_output))
+        logging.debug("inbound: %s,%s,%s; outbound: %s,%s,%s",
+                      average_inbound_from_cmd_output, peak_inbound_from_cmd_output,
+                      burst_inbound_from_cmd_output, average_outbound_from_cmd_output,
+                      peak_outbound_from_cmd_output, burst_outbound_from_cmd_output)
+        if netfloor:
+            logging.debug("inbound floor: %s", floor_inbound_from_cmd_output)
 
     # get inbound and outbound parameters from vm xml
     virt_xml_obj = vm_xml.VMXML(virsh_instance=virsh)
@@ -94,22 +107,25 @@ def check_domiftune(params, test_clear):
         domiftune_params = virt_xml_obj.get_iftune_params(vm_name)
 
     try:
-        logging.debug("test inbound is %s, test outbound is %s" %
-                      (test_inbound, test_outbound))
+        logging.debug("test inbound is %s, test outbound is %s",
+                      test_inbound, test_outbound)
         if test_inbound:
             average_inbound_from_xml = int(domiftune_params.get("inbound").get("average"))
             peak_inbound_from_xml = int(domiftune_params.get("inbound").get("peak"))
             burst_inbound_from_xml = int(domiftune_params.get("inbound").get("burst"))
             logging.debug("inbound from xml:")
-            logging.debug("%s, %s, %s" % (average_inbound_from_xml, peak_inbound_from_xml,
-                                          burst_inbound_from_xml))
+            logging.debug("%s, %s, %s", average_inbound_from_xml, peak_inbound_from_xml,
+                          burst_inbound_from_xml)
+            if netfloor:
+                floor_inbound_from_xml = int(domiftune_params.get("inbound").get("floor"))
+                logging.debug("inbound floor from xml: %s", floor_inbound_from_xml)
         if test_outbound:
             average_outbound_from_xml = int(domiftune_params.get("outbound").get("average"))
             peak_outbound_from_xml = int(domiftune_params.get("outbound").get("peak"))
             burst_outbound_from_xml = int(domiftune_params.get("outbound").get("burst"))
             logging.debug("outbound from xml:")
-            logging.debug("%s, %s, %s" % (average_outbound_from_xml, peak_outbound_from_xml,
-                                          burst_outbound_from_xml))
+            logging.debug("%s, %s, %s", average_outbound_from_xml, peak_outbound_from_xml,
+                          burst_outbound_from_xml)
     except AttributeError as details:
         logging.error("Error in get inbound/outbound average: %s", details)
     logging.debug("average_inbound_from_xml=%s, average_outbound_from_xml=%s",
@@ -152,6 +168,10 @@ def check_domiftune(params, test_clear):
                           average_inbound_from_cmd_output, peak_inbound_from_cmd_output,
                           burst_inbound_from_cmd_output)
             return False
+        if netfloor and inbound_floor != floor_inbound_from_cmd_output:
+            logging.error("To expect inbound floor %s, but got %s from cmd output",
+                          inbound_floor, floor_inbound_from_cmd_output)
+            return False
         if test_inbound and (inbound_average != average_inbound_from_xml
                              or inbound_peak != peak_inbound_from_xml
                              or inbound_burst != burst_inbound_from_xml):
@@ -159,6 +179,10 @@ def check_domiftune(params, test_clear):
                           " %s, burst: %s} from xml", inbound,
                           average_inbound_from_xml, peak_inbound_from_xml,
                           burst_inbound_from_xml)
+            return False
+        if netfloor and inbound_floor != floor_inbound_from_xml:
+            logging.error("To expect inbound floor %s, but got %s from xml",
+                          inbound_floor, floor_inbound_from_xml)
             return False
         if test_outbound and (outbound_average != average_outbound_from_cmd_output
                               or outbound_peak != peak_outbound_from_cmd_output
@@ -205,7 +229,7 @@ def get_domiftune_parameter(params, test, libvirtd):
     options = params.get("options")
     interface = params.get("iface_dev")
 
-    result = virsh.domiftune(vm_name, interface, options=options)
+    result = virsh.domiftune(vm_name, interface, options=options, debug=True)
     status = result.exit_status
 
     # Check status_error
@@ -338,19 +362,57 @@ def run(test, params, env):
     start_vm = params.get("start_vm", "yes")
     change_parameters = params.get("change_parameters", "no")
     interface_ref = params.get("interface_ref", "name")
-
+    netfloor = params.get("netfloor")
+    pre_vmstate = params.get("pre_vmstate")
     interface = []
+    change_net = False
+
+    def set_netbw(netfloor, if_net, netxml):
+        """
+        Set network bandwidth as required.
+
+        :param netfloor: action on network bandwidth, 'need' or 'delete'
+        :param if_net: network name acquired from guest interface
+        :param netxml: the network xml
+        """
+        if netfloor == 'delete':
+            netxml.del_element('/bandwidth')
+        elif netfloor == 'need':
+            netxml.bandwidth_inbound = {'average': 100, 'peak': 200, 'burst': 128}
+            netxml.bandwidth_outbound = {'average': 50, 'peak': 100, 'burst': 128}
+        netxml.sync()
+        virsh.net_dumpxml(if_net, debug=True)
+
+    def get_iface(vm_name, if_mac):
+        """
+        Get the first interface dev from guest xml
+
+        :param vm_name: guest name
+        :param if_mac: the interface mac
+        """
+        vmxml = vm_xml.VMXML.new_from_dumpxml(vm_name)
+        if_node = vmxml.get_iface_all().get(if_mac)
+        return if_node
+
+    if pre_vmstate == "shutoff" and vm.is_alive():
+        vm.destroy()
+
+    if_mac = vm_xml.VMXML.get_first_mac_by_name(vm_name)
+    if netfloor:
+        if_node = get_iface(vm_name, if_mac)
+        if_net = if_node.find('source').get('network')
+        netxml = network_xml.NetworkXML.new_from_net_dumpxml(if_net)
+        netxml_backup = netxml.copy()
+        if netfloor == 'delete' and 'bandwidth' in str(netxml) or \
+                netfloor == 'need' and 'bandwidth' not in str(netxml):
+            set_netbw(netfloor, if_net, netxml)
+            change_net = True
 
     if vm and not vm.is_alive():
         vm.start()
 
     if vm and vm.is_alive():
-        virt_xml_obj = vm_xml.VMXML(virsh_instance=virsh)
-        interface = virt_xml_obj.get_iface_dev(vm_name)
-        if_mac = interface[0]
-        # Get interface name
-        vmxml = virt_xml_obj.new_from_dumpxml(vm_name)
-        if_node = vmxml.get_iface_all().get(if_mac)
+        if_node = get_iface(vm_name, if_mac)
         if_name = if_node.find('target').get('dev')
 
     if interface_ref == "name":
@@ -377,5 +439,14 @@ def run(test, params, env):
         set_domiftune_parameter(test_dict, test, libvirtd)
 
     if change_parameters != "no":
-        ret = virsh.domiftune(vm_name, interface, 'current', '0', '0', debug=True)
+        if change_net:
+            netxml_backup.sync()
+            if vm.is_alive():
+                vm.destroy()
+                vm.start()
+        if not status_error and interface_ref == "mac":
+            opt = 'config'
+        else:
+            opt = 'current'
+        ret = virsh.domiftune(vm_name, if_mac, opt, '0', '0', debug=True)
         libvirt.check_exit_status(ret)
