7aaed98a62055bc1d78507101803bc4db747e45f
lcheng <lcheng@redhat.com>
2021-08-21 00:12:48 +0800

    live_migration: Add new case about poweroff guest
    RHEL-265508 - [Migration] Poweroff inside vm on source host during
    Perform Phase of live migration - p2p migration
    
    Signed-off-by: lcheng <lcheng@redhat.com>

--- provider/migration/migration_base.py
+++ provider/migration/migration_base.py
@@ -195,3 +195,13 @@ def check_event_output(params, test, virsh_session=None, remote_virsh_session=No
     if expected_event_target and remote_virsh_session:
         target_output = remote_virsh_session.get_stripped_output()
         check_output(target_output, eval(expected_event_target), test)
+
+
+def poweroff_src_vm(params):
+    """
+    Poweroff guest on source host
+
+    :param params: dict, used to setup the connection
+    """
+    vm_session = params.get("vm_session")
+    vm_session.cmd("poweroff", ignore_all_errors=True)
