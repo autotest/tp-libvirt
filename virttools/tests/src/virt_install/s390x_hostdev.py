74523a7a989a77625b7d0ed919b2a40e336d92c3
Sebastian Mitterle <smitterl@redhat.com>
2021-11-16 12:33:02 -0500

    Determine image path
    Signed-off-by: Sebastian Mitterle <smitterl@redhat.com>

--- virttools/tests/src/virt_install/s390x_hostdev.py
+++ virttools/tests/src/virt_install/s390x_hostdev.py
@@ -1,10 +1,18 @@
+import os
 import logging
+from avocado.core import data_dir
 from virttest.utils_zcrypt import CryptoDeviceInfoBuilder, \
     APMaskHelper, load_vfio_ap, unload_vfio_ap
 from provider.vfio import ccw
 from uuid import uuid4
+from virttest import storage
 
 def run(test, env, params):
+    disk = storage.get_image_filename_filesytem(env, data_dir.get_data_dir()))
+    
+    pass
+
+def __fake_run(test, env, params):
     """
     Import machine with supported --hostdevs on s390x.
     Start the machine and confirm the passthrough.
@@ -33,6 +41,8 @@ def run(test, env, params):
     if not ccw.device_is_listed(session, chpids):
         test.fail("CCW device not listed")
     """
+    try:
+        pass
     finally:
         if matrix_dev:
             matrix_dev.unassign_all()
