b77620749a56c3e997dbcb7509b92dba45e5665d
cliping <lcheng@redhat.com>
2022-09-03 01:50:08 +0800

    migration: Add case for memory compression
    VIRT-293935 - VM live migration - memory compression(--compressed)
    
    Signed-off-by: cliping <lcheng@redhat.com>

--- libvirt/tests/src/migration/migration_performance_tuning/migration_performance_tuning.py
+++ libvirt/tests/src/migration/migration_performance_tuning/migration_performance_tuning.py
@@ -21,6 +21,26 @@ def run(test, params, env):
         virsh.migrate_setmaxdowntime(vm_name, "100", debug=True)
         migration_obj.setup_connection()
 
+    def verify_memory_compression():
+        """
+        Verify for memory compression
+
+        """
+        cache_size = params.get("cache_size")
+        check_compression_list = eval(params.get("check_compression_list"))
+        test.log.debug("Start verify compression cache.")
+        ret = virsh.domjobinfo(vm_name, extra=" --completed", debug=True, ignore_status=True)
+        if ret.exit_status:
+            test.fail("Failed to get domjobinfo --completed: %s" % ret.stderr)
+        jobinfo = ret.stdout_text
+        for name in check_compression_list:
+            if name not in jobinfo:
+                test.fail("Not found '%s' in domjobinfo." % name)
+        if cache_size:
+            comp_info = "Compression cache: %.3f MiB" % (int(cache_size)/(1024*1024))
+            if comp_info not in jobinfo:
+                test.fail("Not found '%s' in domjobinfo." % comp_info)
+
     test_case = params.get('test_case', '')
     vm_name = params.get("migrate_main_vm")
 
