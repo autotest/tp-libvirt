0c4cac7707f829aa8ba377408f5ab1508d0961cf
nanli <nanli@redhat.com>
2022-09-15 19:31:20 +0800

    fix none type get source file   need skip to get source file if source tage is none Signed-off-by: nanli <nanli@redhat.com>

--- provider/virtual_disk/disk_base.py
+++ provider/virtual_disk/disk_base.py
@@ -70,7 +70,8 @@ class DiskBase(object):
                                elem.find('source').get('name') or
                                elem.find('source').get('dev') or
                                elem.find('source').get('volume')
-                               for elem in backing_list]
+                               for elem in backing_list
+                               if elem.find("source") is not None]
                 source_list.insert(0, active_level_path)
                 break
 
