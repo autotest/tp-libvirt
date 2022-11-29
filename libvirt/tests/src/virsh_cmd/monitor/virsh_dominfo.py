e57c6f2f48edf0bbc3feab019d18430ec01a453a
Anushree Mathur <anushree.mathur2@ibm.com>
2022-11-29 12:13:27 +0530

    Fix test failure expectations when libvirtd is off
    libvirtd is restarted after issuing virsh commands from libvirt
    version 5.6.0 on.
    
    Signed-off-by: Anushree Mathur <anushree.mathur2@ibm.com>

--- libvirt/tests/src/virsh_cmd/monitor/virsh_dominfo.py
+++ libvirt/tests/src/virsh_cmd/monitor/virsh_dominfo.py
@@ -1,8 +1,9 @@
 from virttest import ssh_key
 from virttest import virsh
 from virttest import utils_libvirtd
+import logging
 
-
+from provider import libvirt_version
 def run(test, params, env):
     """
     Test command: virsh dominfo.
@@ -64,7 +65,11 @@ def run(test, params, env):
     # check status_error
     if status_error == "yes":
         if status == 0 or err == "":
-            test.fail("Run successfully with wrong command!")
+            if libvirtd == "off" and libvirt_version.version_compare(5, 6, 0):
+                logging.info("From libvirt version 5.6.0 libvirtd is restarted "
+                             "and command should succeed")
+            else:
+                test.fail("Run successfully with wrong command!")
     elif status_error == "no":
         if status != 0 or output == "":
             test.fail("Run failed with right command")
