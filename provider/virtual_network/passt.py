0b73ced2ed2a749a9053b3d9e8dc151dd7e12d20
Haijiao Zhao <haizhao@redhat.com>
2023-05-31 02:48:15 -0400

    passt:Ignore comment line when comparing nameserver
    Signed-off-by: Haijiao Zhao <haizhao@redhat.com>

--- provider/virtual_network/passt.py
+++ provider/virtual_network/passt.py
@@ -204,7 +204,7 @@ def check_nameserver(session):
 
     :param session: vm shell session instance
     """
-    get_cmd = 'cat /etc/resolv.conf'
+    get_cmd = 'cat /etc/resolv.conf|grep -vE "#|;"'
     on_host = process.run(get_cmd).stdout_text.strip()
     on_vm = session.cmd_output(get_cmd).strip()
     if on_host == on_vm:
