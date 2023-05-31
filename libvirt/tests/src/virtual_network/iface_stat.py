8f4aa1401e73dc9f72a3e54232fff17bb89ab59d
Yalan Zhang <yalzhang@redhat.com>
2023-05-31 05:57:31 -0400

    Add version check for direct type interface
    For direct type interface, the bug fixed since libvirt 9.2.0.
    Add the version check for it.
    
    Signed-off-by: Yalan Zhang <yalzhang@redhat.com>

--- libvirt/tests/src/virtual_network/iface_stat.py
+++ libvirt/tests/src/virtual_network/iface_stat.py
@@ -7,6 +7,7 @@ from avocado.utils import process
 from virttest import utils_misc
 from virttest import utils_net
 from virttest import virsh
+from virttest import libvirt_version
 
 from virttest.libvirt_xml import vm_xml
 from virttest.utils_libvirt import libvirt_vmxml
@@ -110,6 +111,7 @@ def run(test, params, env):
     """
     Test iface stat
     """
+    libvirt_version.is_libvirt_feature_supported(params)
     vm_name = params.get('main_vm')
     case = params.get('case', '')
     scenario = params.get('scenario', '')
