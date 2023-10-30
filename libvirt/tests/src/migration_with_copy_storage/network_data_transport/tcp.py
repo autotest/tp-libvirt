bebb55e3317cf3b96f40876d26e47e71cbdbad0e
lcheng <lcheng@redhat.com>
2023-10-30 15:57:31 +0800

    migration: Add listen address case
    VIRT-297874 - VM live migration with copy storage - network data
    transport - TCP - --listen-address
    
    Signed-off-by: lcheng <lcheng@redhat.com>

---
+++ libvirt/tests/src/migration_with_copy_storage/network_data_transport/tcp.py
@@ -0,0 +1,34 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright Redhat
+#
+#   SPDX-License-Identifier: GPL-2.0
+#
+#   Author: Liping Cheng <lcheng@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from provider.migration import base_steps
+
+
+def run(test, params, env):
+    """
+    Test VM live migration with copy storage - network data transport - TCP.
+
+    :param test: test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment.
+    """
+    vm_name = params.get("migrate_main_vm")
+
+    vm = env.get_vm(vm_name)
+    migration_obj = base_steps.MigrationBase(test, vm, params)
+
+    try:
+        migration_obj.setup_connection()
+        base_steps.prepare_disks_remote(params, vm)
+        migration_obj.run_migration()
+        migration_obj.verify_default()
+    finally:
+        migration_obj.cleanup_connection()
+        base_steps.cleanup_disks_remote(params, vm)
