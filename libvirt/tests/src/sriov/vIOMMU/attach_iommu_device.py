04fe8e34bc2dc957bea2b3779989ac39d11c86ff
Yingshun Cui <yicui@redhat.com>
2024-01-26 14:59:04 +0800

    vIOMMU: Add a case of attach iommu device
    This PR adds:
        VIRT-294818 - Hotplug or coldplug iommu device to guest
    
    Signed-off-by: Yingshun Cui <yicui@redhat.com>

---
+++ libvirt/tests/src/sriov/vIOMMU/attach_iommu_device.py
@@ -0,0 +1,35 @@
+from virttest import libvirt_version
+from virttest import virsh
+
+from virttest.libvirt_xml import vm_xml
+from virttest.utils_libvirt import libvirt_vmxml
+from virttest.utils_test import libvirt
+
+
+def run(test, params, env):
+    """
+    Hotplug or coldplug iommu device to guest
+    """
+    libvirt_version.is_libvirt_feature_supported(params)
+    iommu_dict = eval(params.get('iommu_dict', '{}'))
+    attach_option = params.get("attach_option", "")
+    err_msg = params.get("err_msg")
+
+    vm_name = params.get("main_vm", "avocado-vt-vm1")
+    vm = env.get_vm(vm_name)
+    vmxml = vm_xml.VMXML.new_from_dumpxml(vm_name)
+    backup_vmxml = vmxml.copy()
+    try:
+        if attach_option and vm.is_alive():
+            vm.destroy()
+        elif not attach_option and not vm.is_alive():
+            vm.start()
+            vm.wait_for_login().close()
+
+        iommu_dev = libvirt_vmxml.create_vm_device_by_type('iommu', iommu_dict)
+        test.log.debug(f"iommu device: {iommu_dev}")
+        res = virsh.attach_device(vm.name, iommu_dev.xml, debug=True,
+                                  flagstr=attach_option)
+        libvirt.check_result(res, err_msg)
+    finally:
+        backup_vmxml.sync()
