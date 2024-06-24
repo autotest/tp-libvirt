65b0922aa97a428d301e124d5bd9ebe1b96724ea
lcheng <lcheng@redhat.com>
2024-06-24 18:20:09 +0800

    migration: Add step to check userfaultfd
    1. Add a new step to Check libvirt pass userfaultfd in vm namespace and qemu
    can access this file correctly after postcopy migration on target host.
    2. Remove a redundant file.
    
    Signed-off-by: lcheng <lcheng@redhat.com>

--- libvirt/tests/src/migration/memory_copy_mode/memory_copy_mode.py
+++ libvirt/tests/src/migration/memory_copy_mode/memory_copy_mode.py
@@ -1,3 +1,6 @@
+from virttest import libvirt_version
+from virttest import remote
+
 from provider.migration import base_steps
 from provider.migration import migration_base
 
@@ -10,7 +13,22 @@ def run(test, params, env):
     :param params: Dictionary with the test parameters
     :param env: Dictionary with test environment.
     """
+    def verify_postcopy():
+        """
+        Verify for postcopy migration
+
+        """
+        test.log.info("Verify steps.")
+        migration_obj.verify_default()
+
+        if libvirt_version.version_compare(10, 1, 0):
+            # Check libvirt pass userfaultfd in vm namespace and qemu can access
+            # this file correctly after postcopy migration on target host.
+            cmd = "nsenter -a -t `pidof qemu-kvm` lsof /dev/userfaultfd"
+            remote.run_remote_cmd(cmd, params, ignore_status=False)
+
     vm_name = params.get("migrate_main_vm")
+    test_case = params.get('test_case', '')
 
     virsh_session = None
     remote_virsh_session = None
@@ -18,12 +36,15 @@ def run(test, params, env):
     vm = env.get_vm(vm_name)
     migration_obj = base_steps.MigrationBase(test, vm, params)
 
+    verify_test = eval("verify_%s" % test_case) if "verify_%s" % test_case in \
+        locals() else migration_obj.verify_default
+
     try:
         migration_obj.setup_connection()
         # Monitor event on source/target host
         virsh_session, remote_virsh_session = migration_base.monitor_event(params)
         migration_obj.run_migration()
-        migration_obj.verify_default()
+        verify_test()
         migration_base.check_event_output(params, test, virsh_session, remote_virsh_session)
     finally:
         migration_obj.cleanup_connection()
