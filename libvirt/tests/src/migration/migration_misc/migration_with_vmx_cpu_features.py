5951bf962e832442fb1eaa42025d475e12a70477
lcheng <lcheng@redhat.com>
2025-01-17 11:04:56 +0800

    migration: add case about migrate vm which have vmx-* cpu features
    XXX-303279 - [VM migration] migrate vm which have vmx-* cpu features
    
    Signed-off-by: lcheng <lcheng@redhat.com>

---
+++ libvirt/tests/src/migration/migration_misc/migration_with_vmx_cpu_features.py
@@ -0,0 +1,62 @@
+import os
+
+from avocado.utils import process
+
+from virttest import data_dir
+from virttest import virsh
+from virttest.libvirt_xml import vm_xml
+
+from provider.migration import base_steps
+
+
+def run(test, params, env):
+    """
+    Verify that migration can succeed when host and guest have vmx-* cpu
+    features.
+
+    :param test: test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment.
+    """
+    def setup_test():
+        """
+        Setup steps
+
+        """
+        cpu_mode = params.get("cpu_mode")
+        migration_option = params.get("migration_option")
+
+        test.log.info("Setup steps.")
+        migration_obj.setup_connection()
+        vmxml = vm_xml.VMXML.new_from_dumpxml(vm_name)
+        if cpu_mode == "host_model":
+            vm_attrs = eval(params.get('vm_attrs', '{}'))
+            vmxml.setup_attrs(**vm_attrs)
+            vmxml.sync()
+        else:
+            base_steps.sync_cpu_for_mig(params)
+
+        if not vm.is_alive():
+            vm.start()
+            vm.wait_for_login().close()
+
+        if migration_option == "with_xml":
+            xmlfile = os.path.join(data_dir.get_tmp_dir(), '%s.xml' % vm_name)
+            virsh.dumpxml(vm_name, extra="--migratable", to_file=xmlfile, ignore_status=False)
+            params.update({"virsh_migrate_extra": f"--xml {xmlfile}"})
+
+    vm_name = params.get("migrate_main_vm")
+
+    vm = env.get_vm(vm_name)
+    migration_obj = base_steps.MigrationBase(test, vm, params)
+
+    cmd = "virsh domcapabilities |grep vmx- |wc -l"
+    ret = process.run(cmd, shell=True).stdout_text.strip()
+    if int(ret) == 0:
+        test.cancel("Please use the host with vmx-* cpu features to test.")
+    try:
+        setup_test()
+        migration_obj.run_migration()
+        migration_obj.verify_default()
+    finally:
+        migration_obj.cleanup_connection()
