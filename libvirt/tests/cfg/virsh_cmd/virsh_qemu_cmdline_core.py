f5fc92f4b4165a2afe15b5736a339f99bb309bbd
Kowshik Jois B S <kowsjois@linux.ibm.com>
2025-01-28 14:28:36 +0530

    Fixed check errors
    Signed-off-by: Kowshik Jois B S <kowsjois@linux.ibm.com>

---
+++ libvirt/tests/cfg/virsh_cmd/virsh_qemu_cmdline_core.py
@@ -0,0 +1,30 @@
+import subprocess
+
+def run(test, params, env):
+
+    qemu_binary = params.get("qemu_binary")
+    errors = ["tcg_region_init", "assertion failed", "Aborted",
+              "core dumped", "Invalid CPU topology"]
+
+    def check_qemu_cmdline(cpus=9999):
+
+        print("checking with %d CPUs" % cpus)
+
+        command = "%s -accel tcg -smp 10,maxcpus=%d" % (qemu_binary, cpus)
+        global output
+        output = subprocess.getoutput(command)
+
+        for err in errors:
+            if err in output:
+                return True
+        return False
+
+    cpus = ["9000", "123", "97865", "56789", "123456789"]
+
+    for cpu in cpus:
+        failed = check_qemu_cmdline(int(cpu))
+        if failed:
+            break
+
+    if failed:
+        test.fail(output)
