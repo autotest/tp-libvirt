7afbbd98ef7e0e4e7d78cad7067f22a82952dec1
Sebastian Mitterle <smitterl@redhat.com>
2025-07-10 04:38:56 -0400

    virsh_nodedev_list: apply formatting
    apply black formatting to comply with CI check
    
    Signed-off-by: Sebastian Mitterle <smitterl@redhat.com>

--- libvirt/tests/src/virsh_cmd/nodedev/virsh_nodedev_list.py
+++ libvirt/tests/src/virsh_cmd/nodedev/virsh_nodedev_list.py
@@ -12,7 +12,7 @@ from virttest import utils_split_daemons
 
 # Using as lower capital is not the best way to do, but this is just a
 # workaround to avoid changing the entire file.
-logging = log.getLogger('avocado.' + __name__)
+logging = log.getLogger("avocado." + __name__)
 
 
 def get_avail_caps(all_caps):
@@ -38,15 +38,15 @@ def get_scm_devices():
     """
     scm_devices = []
     try:
-        utils_path.find_command('lsscm')
-        scm_output = process.run('lsscm', ignore_status=True, shell=True).stdout_text
-        scm_list = scm_output.split('\n')[2:-1]
+        utils_path.find_command("lsscm")
+        scm_output = process.run("lsscm", ignore_status=True, shell=True).stdout_text
+        scm_list = scm_output.split("\n")[2:-1]
         for scm in scm_list:
-            scm_info_list = [info for info in scm.split(' ') if info != '']
-            scm_devices.append('block_' + scm_info_list[2])
+            scm_info_list = [info for info in scm.split(" ") if info != ""]
+            scm_devices.append("block_" + scm_info_list[2])
     except utils_path.CmdNotFoundError:
-        logging.warning('Cmd lsscm not found!')
-        logging.warning('You can try to install it by `yum install s390utils*`')
+        logging.warning("Cmd lsscm not found!")
+        logging.warning("You can try to install it by `yum install s390utils*`")
     return scm_devices
 
 
@@ -59,35 +59,36 @@ def get_storage_devices():
     """
     devices = []
     try:
-        utils_path.find_command('udevadm')
-        storage_path = '/sys/class/block'
+        utils_path.find_command("udevadm")
+        storage_path = "/sys/class/block"
         if not os.path.exists(storage_path):
-            logging.debug(
-                'Storage device path %s doesn`t exists!', storage_path)
+            logging.debug("Storage device path %s doesn`t exists!", storage_path)
             return []
         for device in os.listdir(storage_path):
             info = process.run(
-                               'udevadm info %s' % os.path.join(storage_path, device),
-                               timeout=5, ignore_status=True, shell=True).stdout_text
+                "udevadm info %s" % os.path.join(storage_path, device),
+                timeout=5,
+                ignore_status=True,
+                shell=True,
+            ).stdout_text
             # Only disk devices are list, not partition
-            dev_type = re.search(r'(?<=E: DEVTYPE=)\S*', info)
-            dev_real_virtual = re.search(r'P: /devices/virtual', info)
+            dev_type = re.search(r"(?<=E: DEVTYPE=)\S*", info)
+            dev_real_virtual = re.search(r"P: /devices/virtual", info)
             if dev_type and not dev_real_virtual:
-                if dev_type.group(0) == 'disk':
+                if dev_type.group(0) == "disk":
                     # Get disk serial
-                    dev_id = re.search(r'(?<=E: ID_SERIAL=)\S*', info)
+                    dev_id = re.search(r"(?<=E: ID_SERIAL=)\S*", info)
                     if dev_id:
                         serial = dev_id.group(0)
-                        dev_name = 'block_' + device.replace(':', '_')
-                        dev_name = re.sub(
-                            r'\W', '_', 'block_%s_%s' % (device, serial))
+                        dev_name = "block_" + device.replace(":", "_")
+                        dev_name = re.sub(r"\W", "_", "block_%s_%s" % (device, serial))
                         devices.append(dev_name)
                     else:
-                        dev_name = 'block_' + device.replace(':', '_')
+                        dev_name = "block_" + device.replace(":", "_")
                         devices.append(dev_name)
     except utils_path.CmdNotFoundError:
-        logging.warning('udevadm not found! Skipping storage test!')
-        logging.warning('You can try install it using `yum install udev`')
+        logging.warning("udevadm not found! Skipping storage test!")
+        logging.warning("You can try install it using `yum install udev`")
     return devices
 
 
@@ -98,33 +99,33 @@ def get_net_devices():
     :return:      A list contains retrieved net device names with
                   the same format in virsh.
     """
-    net_path = '/sys/class/net'
+    net_path = "/sys/class/net"
     devices = []
     if os.path.exists(net_path):
         for device in os.listdir(net_path):
-            if device == 'bonding_masters':
+            if device == "bonding_masters":
                 continue
             try:
-                dev_dir = os.path.join('/sys/class/net', device)
+                dev_dir = os.path.join("/sys/class/net", device)
                 # Ignore bridge devices
-                if os.path.exists(os.path.join(dev_dir, 'bridge')):
+                if os.path.exists(os.path.join(dev_dir, "bridge")):
                     continue
                 # Ignore bonding devices
-                if os.path.exists(os.path.join(dev_dir, 'bonding')):
+                if os.path.exists(os.path.join(dev_dir, "bonding")):
                     continue
                 # Ignore vlan devices
-                if [x for x in os.listdir(dev_dir) if x.startswith('lower')]:
+                if [x for x in os.listdir(dev_dir) if x.startswith("lower")]:
                     continue
-                address_file = os.path.join(dev_dir, 'address')
-                mac = ''
-                with open(address_file, 'r') as f_addr:
+                address_file = os.path.join(dev_dir, "address")
+                mac = ""
+                with open(address_file, "r") as f_addr:
                     mac = f_addr.read().strip()
             except IOError:
-                print('Cannot get address for device %s' % device)
+                print("Cannot get address for device %s" % device)
             if mac:
-                dev_name = re.sub(r'\W', '_', 'net_%s_%s' % (device, mac))
+                dev_name = re.sub(r"\W", "_", "net_%s_%s" % (device, mac))
             else:
-                dev_name = re.sub(r'\W', '_', 'net_%s' % device)
+                dev_name = re.sub(r"\W", "_", "net_%s" % device)
             devices.append(dev_name)
     return devices
 
@@ -135,21 +136,21 @@ def get_ccw_io_devices():
 
     :return: A list of ccw devices in the same format as nodedev-list
     """
-    ccw_base_path = '/sys/bus/ccw/devices'
+    ccw_base_path = "/sys/bus/ccw/devices"
     devices = []
     if not os.path.exists(ccw_base_path):
         return devices
     for device in os.listdir(ccw_base_path):
         online_attribute = os.path.join(ccw_base_path, device, "online")
-        online_value = process.run("cat %s" % online_attribute,
-                                   timeout=5, ignore_status=True,
-                                   shell=True).stdout_text
+        online_value = process.run(
+            "cat %s" % online_attribute, timeout=5, ignore_status=True, shell=True
+        ).stdout_text
         is_online = "1" == online_value.strip()
         # After libvirt version 10.5.0, virsh nodedev list will list all of ccw devices regardless of its state
         if libvirt_version.version_compare(10, 5, 0):
             is_online = True
         if is_online:
-            dev_name = re.sub(r'\W', '_', 'ccw_' + device)
+            dev_name = re.sub(r"\W", "_", "ccw_" + device)
             devices.append(dev_name)
     return devices
 
@@ -160,17 +161,17 @@ def get_css_io_devices():
 
     :return: A list of css devices in the same format as nodedev-list
     """
-    css_base_path = '/sys/bus/css'
-    io_subchannel_path = '/sys/bus/css/drivers/io_subchannel'
-    vfio_ccw_path = '/sys/bus/css/drivers/vfio_ccw'
+    css_base_path = "/sys/bus/css"
+    io_subchannel_path = "/sys/bus/css/drivers/io_subchannel"
+    vfio_ccw_path = "/sys/bus/css/drivers/vfio_ccw"
     devices = []
     if os.path.exists(css_base_path):
         for path in [io_subchannel_path, vfio_ccw_path]:
             if not os.path.exists(path):
                 continue
             for device in os.listdir(path):
-                if re.match(r'\d+\.\d+\.\d+', device):
-                    dev_name = re.sub(r'\W', '_', 'css_' + device)
+                if re.match(r"\d+\.\d+\.\d+", device):
+                    dev_name = re.sub(r"\W", "_", "css_" + device)
                     devices.append(dev_name)
     return devices
 
@@ -192,17 +193,23 @@ def get_devices_by_cap(cap):
                   implemented, an empty list is returned.
     """
     cap_map = {
-        'pci': ('/sys/bus/pci/devices/', 'pci_', '.*'),
-        'scsi_host': ('/sys/class/scsi_host', 'scsi_', '.*'),
-        'scsi': ('/sys/class/scsi_device', 'scsi_', '.*'),
-        'scsi_generic': ('/sys/class/scsi_generic', 'scsi_generic_', '.*'),
-        'scsi_target': ('/sys/bus/scsi/devices', 'scsi_', r'target.*'),
-        'usb': ('/sys/bus/usb/devices', 'usb_',
-                # Match any string that does not have :X.X at the end
-                r'\S+:\d+\.\d+'),
-        'usb_device': ('/sys/bus/usb/devices', 'usb_',
-                       # Match any string that does not have :X.X at the end
-                       r'^((?!\S+:\d+\.\d+).)*$')
+        "pci": ("/sys/bus/pci/devices/", "pci_", ".*"),
+        "scsi_host": ("/sys/class/scsi_host", "scsi_", ".*"),
+        "scsi": ("/sys/class/scsi_device", "scsi_", ".*"),
+        "scsi_generic": ("/sys/class/scsi_generic", "scsi_generic_", ".*"),
+        "scsi_target": ("/sys/bus/scsi/devices", "scsi_", r"target.*"),
+        "usb": (
+            "/sys/bus/usb/devices",
+            "usb_",
+            # Match any string that does not have :X.X at the end
+            r"\S+:\d+\.\d+",
+        ),
+        "usb_device": (
+            "/sys/bus/usb/devices",
+            "usb_",
+            # Match any string that does not have :X.X at the end
+            r"^((?!\S+:\d+\.\d+).)*$",
+        ),
     }
     if cap in cap_map:
         devices = []
@@ -210,17 +217,17 @@ def get_devices_by_cap(cap):
         if os.path.exists(path):
             for device in os.listdir(path):
                 if re.match(pattern, device):
-                    dev_name = re.sub(r'\W', '_', header + device)
+                    dev_name = re.sub(r"\W", "_", header + device)
                     devices.append(dev_name)
-    elif cap == 'system':
-        devices = ['machine']
-    elif cap == 'net':
+    elif cap == "system":
+        devices = ["machine"]
+    elif cap == "net":
         devices = get_net_devices()
-    elif cap == 'storage':
+    elif cap == "storage":
         devices = get_storage_devices()
-    elif cap == 'css':
+    elif cap == "css":
         devices = get_css_io_devices()
-    elif cap == 'ccw':
+    elif cap == "ccw":
         devices = get_ccw_io_devices()
     else:
         devices = []
@@ -259,6 +266,7 @@ def run(test, params, env):
     2) If `cap_option == one`, results are also compared
        with devices get from sysfs.
     """
+
     def _check_result(cap, ref_list, result, mode, pattern):
         """
         Check test result against a device list retrieved from sysfs.
@@ -279,24 +287,37 @@ def run(test, params, env):
             check_list = _substitute_with_match(pattern, check_list)
         are_not_equivalent = set(ref_list) != set(check_list)
 
-        uavail_caps = ['system', 'vports', 'fc_host']
+        uavail_caps = ["system", "vports", "fc_host"]
 
         if are_not_equivalent and cap not in uavail_caps:
-            logging.error('Difference in capability %s:', cap)
-            logging.error('Expected devices: %s', ref_list)
-            logging.error('Result devices  : %s', check_list)
+            logging.error("Difference in capability %s:", cap)
+            logging.error("Expected devices: %s", ref_list)
+            logging.error("Result devices  : %s", check_list)
             return False
         return True
 
     mode = params.get("comparison_mode", "exact")
     pattern = params.get("comparison_pattern", "exact")
-    remove_scm_device = 'yes' == params.get("remove_scm_device", "no")
-    all_caps = ['system', 'pci', 'usb_device', 'usb', 'net', 'scsi_host',
-                'scsi_target', 'scsi', 'storage', 'fc_host', 'vports',
-                'scsi_generic', 'ccw', 'css']
-    expect_succeed = params.get('expect_succeed', 'yes')
-    tree_option = params.get('tree_option', 'off')
-    cap_option = params.get('cap_option', 'off')
+    remove_scm_device = "yes" == params.get("remove_scm_device", "no")
+    all_caps = [
+        "system",
+        "pci",
+        "usb_device",
+        "usb",
+        "net",
+        "scsi_host",
+        "scsi_target",
+        "scsi",
+        "storage",
+        "fc_host",
+        "vports",
+        "scsi_generic",
+        "ccw",
+        "css",
+    ]
+    expect_succeed = params.get("expect_succeed", "yes")
+    tree_option = params.get("tree_option", "off")
+    cap_option = params.get("cap_option", "off")
     caps = get_avail_caps(all_caps)
     check_failed = False
 
@@ -304,65 +325,65 @@ def run(test, params, env):
     uri = params.get("virsh_uri")
     if uri and not utils_split_daemons.is_modular_daemon():
         uri = "qemu:///system"
-    unprivileged_user = params.get('unprivileged_user')
+    unprivileged_user = params.get("unprivileged_user")
     if unprivileged_user:
-        if unprivileged_user.count('EXAMPLE'):
-            unprivileged_user = 'testacl'
+        if unprivileged_user.count("EXAMPLE"):
+            unprivileged_user = "testacl"
 
     if not libvirt_version.version_compare(1, 1, 1):
-        if params.get('setup_libvirt_polkit') == 'yes':
-            test.cancel("API acl test not supported in current"
-                        " libvirt version.")
+        if params.get("setup_libvirt_polkit") == "yes":
+            test.cancel("API acl test not supported in current" " libvirt version.")
 
     virsh_dargs = {}
-    if params.get('setup_libvirt_polkit') == 'yes':
-        virsh_dargs['unprivileged_user'] = unprivileged_user
-        virsh_dargs['uri'] = uri
+    if params.get("setup_libvirt_polkit") == "yes":
+        virsh_dargs["unprivileged_user"] = unprivileged_user
+        virsh_dargs["uri"] = uri
 
-    tree = (tree_option == 'on')
-    if cap_option == 'one':
+    tree = tree_option == "on"
+    if cap_option == "one":
         devices = {}
         for cap in caps:
             devices[cap] = get_devices_by_cap(cap)
 
         for cap in devices:
-            logging.debug(cap + ':')
+            logging.debug(cap + ":")
             for device in devices[cap]:
-                logging.debug('    ' + device)
+                logging.debug("    " + device)
 
         for cap in caps:
             result = virsh.nodedev_list(tree=tree, cap=cap, **virsh_dargs)
-            if result.exit_status != 0 and expect_succeed == 'yes':
+            if result.exit_status != 0 and expect_succeed == "yes":
                 break
-            elif result.exit_status == 0 and expect_succeed == 'no':
+            elif result.exit_status == 0 and expect_succeed == "no":
                 break
-            if cap == 'storage' and remove_scm_device:
+            if cap == "storage" and remove_scm_device:
                 scm_devices = get_scm_devices()
                 if scm_devices:
-                    devices[cap] = [device for device in devices[cap]
-                                    if device not in scm_devices]
-            if not _check_result(cap, devices[cap], result.stdout.strip(), mode, pattern):
+                    devices[cap] = [
+                        device for device in devices[cap] if device not in scm_devices
+                    ]
+            if not _check_result(
+                cap, devices[cap], result.stdout.strip(), mode, pattern
+            ):
                 check_failed = True
                 break
     else:
-        cap = ''
-        if cap_option != 'off':
-            if cap_option == 'multi':
-                cap = ','.join(caps)
-            elif cap_option == 'long':
-                cap = ','.join(['pci', 'usb', 'net', 'storage', 'scsi'] * 5000)
+        cap = ""
+        if cap_option != "off":
+            if cap_option == "multi":
+                cap = ",".join(caps)
+            elif cap_option == "long":
+                cap = ",".join(["pci", "usb", "net", "storage", "scsi"] * 5000)
             else:
                 cap = cap_option
         result = virsh.nodedev_list(tree=tree, cap=cap, **virsh_dargs)
 
     logging.debug(result)
-    if expect_succeed == 'yes':
+    if expect_succeed == "yes":
         if result.exit_status != 0:
-            test.fail(
-                'Expected succeed, but failed with result:\n%s' % result)
-    elif expect_succeed == 'no':
+            test.fail("Expected succeed, but failed with result:\n%s" % result)
+    elif expect_succeed == "no":
         if result.exit_status == 0:
-            test.fail(
-                'Expected fail, but succeed with result:\n%s' % result)
+            test.fail("Expected fail, but succeed with result:\n%s" % result)
     if check_failed:
-        test.fail('Check failed. result:\n%s' % result)
+        test.fail("Check failed. result:\n%s" % result)
