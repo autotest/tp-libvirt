acbe81f5043a20b9d86fa8dd28ebf30c46820685
lcheng <lcheng@redhat.com>
2025-10-29 15:55:27 +0800

    migration: Add case to test migrate VM with heavy mem load in VM
    This case is to test vm live migration when there is high memory
    load in vm. This case covers two situations:
    1. vm migration can't finish after many iterations, then user
    cancel migration
    2. vm migration can't finish after many iterations, then user switch
    migration to postcopy
    
    VIRT-298235 - [VM migration] migrate vm with heavy memory load in vm
    
    Signed-off-by: lcheng <lcheng@redhat.com>

---
+++ libvirt/tests/src/migration/migration_with_stress/mem_load.py
@@ -0,0 +1,29 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright Redhat
+#
+#   SPDX-License-Identifier: GPL-2.0
+#
+#   Author: Liping Cheng <lcheng@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from provider.migration import base_steps
+
+
+def run(test, params, env):
+    """
+    Test vm live migration when there is high memory load in vm.
+
+    """
+    vm_name = params.get("migrate_main_vm")
+
+    vm = env.get_vm(vm_name)
+    migration_obj = base_steps.MigrationBase(test, vm, params)
+
+    try:
+        migration_obj.setup_connection()
+        migration_obj.run_migration()
+        migration_obj.verify_default()
+    finally:
+        migration_obj.cleanup_connection()
