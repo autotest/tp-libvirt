a081a4b1c3f5a979eb57f138a87ffac1459ddab9
Wenli Quan <wquan@redhat.com>
2026-01-22 20:38:21 +0800

    Migrate pktgen perf tests from tp-qemu to tp-libvirt
    - Add pktgen_perf_test.cfg: new perf test config with tx/rx, multiple scripts,
      and vhost_vdpa variant (dev_type=vdpa, iface_dict, client, pktgen_tx_dst_mac).
    - Extend pktgen_burst_mode_test.py: vhost-vdpa setup and session-based verify_dmesg.
    - Extend pktgen_utils.py: vdpa/rx/client support
    
    Signed-off-by: Wenli Quan <wquan@redhat.com>

--- libvirt/tests/src/virtual_network/qemu/pktgen_burst_mode_test.py
+++ libvirt/tests/src/virtual_network/qemu/pktgen_burst_mode_test.py
@@ -12,12 +12,16 @@ import os
 from avocado.utils import distro
 from avocado.utils import process
 
+from virttest import virsh
 from virttest import utils_misc
 from virttest import utils_net
 from virttest.libvirt_xml import vm_xml
+from virttest.utils_libvirt import libvirt_vmxml
+from virttest.utils_test import libvirt
 
 from provider.virtual_network import pktgen_utils
 from provider.virtual_network import network_base
+from provider.interface import interface_base
 
 
 def write_to_result_file(test, params):
@@ -28,12 +32,15 @@ def write_to_result_file(test, params):
     :return result_file, pktgen test result file.
     """
     kvm_ver_chk_cmd = params.get("kvm_ver_chk_cmd")
+    libvirt_ver_chk_cmd = params.get("libvirt_ver_chk_cmd")
 
     result_path = utils_misc.get_path(test.resultsdir, "pktgen_perf.RHS")
     result_file = open(result_path, "w")
     kvm_ver = process.system_output(kvm_ver_chk_cmd, shell=True).decode()
+    libvirt_ver = process.system_output(libvirt_ver_chk_cmd, shell=True).decode()
     host_ver = os.uname().release
     result_file.write("### kvm-userspace-ver : %s\n" % kvm_ver)
+    result_file.write("### libvirt-userspace-ver : %s\n" % libvirt_ver)
     result_file.write("### kvm_version : %s\n" % host_ver)
 
     return result_file
@@ -61,10 +68,36 @@ def run(test, params, env):
                 pktgen_utils.install_package(host_ver)
 
         test.log.debug("TEST_STEP 2: Test with guest and host connectivity")
+        recreate_serial_console = False
+        # vhost-vdpa needs XML updates before starting VM
+        if params.get("dev_type") == "vdpa" and params.get("iface_dict"):
+            # Ensure VM is stopped before XML changes
+            virsh.destroy(vm_name, debug=True, ignore_status=True)
+            recreate_serial_console = True
+
+            libvirt_vmxml.remove_vm_devices_by_type(vm, "interface")
+            vm_attrs = eval(params.get("vm_attrs", "{}"))
+            if vm_attrs:
+                vmxml = vm_xml.VMXML.new_from_dumpxml(vm_name)
+                vmxml.setup_attrs(**vm_attrs)
+                vmxml.sync()
+
+            # Add a single vhost-vdpa interface
+            vmxml = vm_xml.VMXML.new_from_dumpxml(vm_name)
+            iface_dict = interface_base.parse_iface_dict(params)
+            iface_dev = interface_base.create_iface(params.get("dev_type"), iface_dict)
+            libvirt.add_vm_device(vmxml, iface_dev)
+
+            if vm.virtnet:
+                vm.virtnet[0].mac = params.get("mac_addr")
+                vm.virtnet[0].nettype = "vdpa"
+
         if not vm.is_alive():
             vm.start()
         test.log.debug("Test with Guest xml:%s", vm_xml.VMXML.new_from_dumpxml(vm_name))
-        session = vm.wait_for_serial_login(restart_network=True)
+        session = vm.wait_for_serial_login(
+            restart_network=True, recreate_serial_console=recreate_serial_console
+        )
         network_base.ping_check(params, ips, session, force_ipv4=True)
 
         test.log.debug("TEST_STEP 3: Install package and Run pktgen in burst mode.")
@@ -86,7 +119,15 @@ def run(test, params, env):
             params, result_file, test_vm, vm, session)
 
         test.log.debug("TEST_STEP: Check the guest dmesg without error messages.")
-        vm.verify_dmesg()
+        if params.get("dev_type") == "vdpa":
+            # vhost-vdpa may not have a guest IP (ARP/arping), so vm.verify_dmesg()
+            # can fail by attempting a network login.
+            utils_misc.verify_dmesg(
+                ignore_result=False,
+                session=session,
+            )
+        else:
+            vm.verify_dmesg()
         vm.verify_kernel_crash()
 
         result_file.close()
