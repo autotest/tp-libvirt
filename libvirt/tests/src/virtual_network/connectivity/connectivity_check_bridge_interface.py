c8c473d3e0d1abf1713bc1dea40ac3e7bce3e4f2
Lei Yang <leiyang@redhat.com>
2026-02-06 13:44:01 +0800

    Improve connectivity_check_bridge_interface to make it work under kar
    1.Add some parameters to allow the framework provide guest xml
    2.Remove the ovs and linux_br variants, let them created by the test
    loop's environment setup part
    3.Add some codes to support ovs bridge type
    
    Signed-off-by: Lei Yang <leiyang@redhat.com>

--- libvirt/tests/src/virtual_network/connectivity/connectivity_check_bridge_interface.py
+++ libvirt/tests/src/virtual_network/connectivity/connectivity_check_bridge_interface.py
@@ -40,17 +40,30 @@ def run(test, params, env):
     vm_name = params.get('main_vm')
     vms = params.get('vms').split()
     vm, ep_vm = (env.get_vm(vm_i) for vm_i in vms)
+    login_timeout = params.get_numeric('login_timeout')
     scenario = params.get('scenario')
     outside_ip = params.get('outside_ip')
     rand_id = utils_misc.generate_random_string(3)
-    bridge_type = params.get('bridge_type', '')
-    bridge_name = bridge_type + '_' + rand_id
+    netdst = params.get('netdst', 'virbr0')
+    bridge_name = netdst.split(',')[0]
     iface_attrs = parse_attrs('iface_attrs', params)
     iface_attrs['source']['bridge'] = bridge_name
     vm_attrs = eval(params.get('vm_attrs', '{}'))
     nwfilter_attrs = parse_attrs('nwfilter_attrs', params)
     iface_in_vm = params.get('iface_in_vm')
     iface_in_vm = iface_in_vm if iface_in_vm else 'eno'
+
+    # Add virtualport for OVS bridges
+    if 'ovs' in bridge_name.lower():
+        iface_attrs['virtualport'] = {'type': 'openvswitch'}
+
+    if nwfilter_attrs:
+        try:
+            br_backend = utils_net.find_bridge_manager(bridge_name)
+            if not isinstance(br_backend, utils_net.Bridge):
+                test.cancel("Host does not use Linux Bridge")
+        except process.CmdError:
+            pass
 
     host_iface = params.get('host_iface')
     host_iface = host_iface if host_iface else utils_net.get_default_gateway(
@@ -59,12 +72,6 @@ def run(test, params, env):
     bkxmls = list(map(vm_xml.VMXML.new_from_inactive_dumpxml, vms))
 
     try:
-        if bridge_type == 'linux_br':
-            utils_net.create_linux_bridge_tmux(bridge_name, host_iface)
-            process.run(f'ip l show type bridge {bridge_name}', shell=True)
-        elif bridge_type == 'ovs_br':
-            utils_net.create_ovs_bridge(bridge_name)
-
         if nwfilter_attrs and not libvirt_version.version_compare(8, 0, 0):
             nwf = nwfilter_xml.NwfilterXML()
             nwf.setup_attrs(**nwfilter_attrs)
@@ -90,11 +97,16 @@ def run(test, params, env):
         libvirt_vmxml.modify_vm_device(
             ep_vmxml, 'interface', {**iface_attrs, **{'mac_address': ep_mac}})
 
+        # Clean the console session created by create_vm_libvirt=yes BEFORE starting VMs
+        for vm_inst in [vm, ep_vm]:
+            if vm_inst.serial_console:
+                vm_inst.cleanup_serial_console()
+
         [LOG.debug(f'VMXML of {vm_x}:\n{virsh.dumpxml(vm_x).stdout_text}')
          for vm_x in vms]
 
         [vm_i.start() for vm_i in [vm, ep_vm]]
-        session, ep_session = (vm_inst.wait_for_serial_login()
+        session, ep_session = (vm_inst.wait_for_serial_login(timeout=login_timeout)
                                for vm_inst in [vm, ep_vm])
 
         ips_v4 = network_base.get_test_ips(session, mac, ep_session, ep_mac,
@@ -165,9 +177,5 @@ def run(test, params, env):
 
     finally:
         [backup_xml.sync() for backup_xml in bkxmls]
-        if bridge_type == 'linux_br':
-            utils_net.delete_linux_bridge_tmux(bridge_name, host_iface)
-        if bridge_type == 'ovs_br':
-            utils_net.delete_ovs_bridge(bridge_name)
         if nwfilter_attrs:
             virsh.nwfilter_undefine(nwfilter_attrs['filter_name'])
